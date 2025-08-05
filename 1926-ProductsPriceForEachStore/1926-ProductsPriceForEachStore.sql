a9fad570b0e779218bbb26a4eef77c9a72926363
Zax S <zacks.shen@gmail.com>
2025-08-04 22:44:02 -0400

    Create 1926-ProductsPriceForEachStore

---
+++ 1926-ProductsPriceForEachStore/1926-ProductsPriceForEachStore.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/4/2025, 10:44:01 PM
+# Write your MySQL query statement below
+
+SELECT product_id,
+    SUM(IF(store = 'store1', price, NULL)) AS store1,
+    SUM(IF(store = 'store2', price, NULL)) AS store2,
+    SUM(IF(store = 'store3', price, NULL)) AS store3
+FROM Products
+GROUP BY product_id;
