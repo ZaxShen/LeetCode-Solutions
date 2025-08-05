8e7e43898a7e0ef5d857fd7ecf259de3fa8e34a3
Zax S <zacks.shen@gmail.com>
2025-08-04 22:45:51 -0400

    Create 1297-MaximumNumberOfBalloons

---
+++ 1297-MaximumNumberOfBalloons/1297-MaximumNumberOfBalloons.py
@@ -0,0 +1,10 @@
+# Last updated: 8/4/2025, 10:45:51 PM
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = {'a': 0, 'b': 0, 'l': 0, 'o': 0, 'n': 0}
+
+        for c in text:
+            if c in d:
+                d[c] += 1
+
+        return min(min(d['o'] // 2, d['l'] // 2), min(d.values()))
