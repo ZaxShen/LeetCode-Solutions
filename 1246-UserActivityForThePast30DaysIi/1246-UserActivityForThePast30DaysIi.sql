acb4bc9a5ec23088e81cef719738cfc6be6d0b00
Zax S <zacks.shen@gmail.com>
2025-08-04 22:45:58 -0400

    Create 1246-UserActivityForThePast30DaysIi

---
+++ 1246-UserActivityForThePast30DaysIi/1246-UserActivityForThePast30DaysIi.sql
@@ -0,0 +1,7 @@
+-- Last updated: 8/4/2025, 10:45:58 PM
+# Write your MySQL query statement below
+SELECT IFNULL(
+	ROUND(COUNT(DISTINCT session_id)/COUNT(DISTINCT user_id), 2), 
+    0) AS average_sessions_per_user
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) < 30;
