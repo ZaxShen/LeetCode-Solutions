d6e9aa044394e7d4440db967c79e4d23316051b1
Zax S <zacks.shen@gmail.com>
2025-08-04 22:43:36 -0400

    Create 2381-DynamicPivotingOfATable

---
+++ 2381-DynamicPivotingOfATable/2381-DynamicPivotingOfATable.sql
@@ -0,0 +1,20 @@
+-- Last updated: 8/4/2025, 10:43:36 PM
+create procedure PivotProducts()
+begin
+    set session group_concat_max_len = 1000000; # default is 1024
+    
+	set @sql = null;
+	select group_concat(
+		distinct concat(
+			'sum(if(store = "', store, '", price, null)) as ', store
+		)
+	)
+	into @sql
+	from Products;
+
+	set @sql = concat('select product_id, ', @sql, ' from Products group by 1');
+
+	prepare stmt from @sql;
+	execute stmt;
+	deallocate prepare stmt;
+end
