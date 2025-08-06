2b8882b7c7e427ae18ab3115b49d15c8fb29f812
Zax S <zacks.shen@gmail.com>
2025-08-06 15:26:39 -0400

    Update 125-ValidPalindrome_20250806_152513.py

--- 125-ValidPalindrome/version/python/125-ValidPalindrome_20250806_152513.py
+++ 125-ValidPalindrome/version/python/125-ValidPalindrome_20250806_152513.py
@@ -2,5 +2,5 @@
 # O(n), O(n)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = (''.join(i.lower() for i in s if i.isalnum()))
+        s = ''.join(i.lower() for i in s if i.isalnum())
         return s == s[::-1]
