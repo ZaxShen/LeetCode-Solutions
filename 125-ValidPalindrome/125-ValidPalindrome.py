6fa008a9f1183be9c511be2b0fd7bccfdab9523d
Zax S <zacks.shen@gmail.com>
2025-08-07 23:10:50 -0400

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,15 @@
+# Last updated: 8/7/2025, 11:10:50 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s)-1
+        while i <= j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            else:
+                if s[i].lower() != s[j].lower():
+                    return False
+                i += 1
+                j -= 1
+        return True
