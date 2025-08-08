dd3b55b6639b575fc5b1f4ec81e552d4c7da2558
Zax S <zacks.shen@gmail.com>
2025-08-08 01:49:03 -0400

    File updated at 8/8/2025, 1:49:03 AM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,12 +1,12 @@
-# Last updated: 8/8/2025, 1:47:13 AM
+# Last updated: 8/8/2025, 1:49:03 AM
 class Solution:
+    # O(n), O(1)
     def sortedSquares(self, nums: List[int]) -> List[int]:
         i, j = 0, len(nums)-1
         p = len(nums) - 1
         res = [0] * len(nums)
         while i <= j:
             i_square, j_square = nums[i]**2, nums[j]**2
-            print(f"i: {i}, {i_square}; j: {j}, {j_square}")
             if i_square < j_square:
                 res[p] = j_square
                 j -= 1
