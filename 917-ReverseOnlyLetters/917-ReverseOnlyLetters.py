3b70aee39872ac1c10b5b04726bf7cddb96e923d
Zax S <zacks.shen@gmail.com>
2025-08-10 14:52:58 -0400

    File updated at 8/10/2025, 2:52:57 PM

--- 917-ReverseOnlyLetters/917-ReverseOnlyLetters.py
+++ 917-ReverseOnlyLetters/917-ReverseOnlyLetters.py
@@ -1,17 +1,18 @@
-# Last updated: 8/10/2025, 2:52:50 PM
+# Last updated: 8/10/2025, 2:52:57 PM
 class Solution:
-	# O(n), O(n)
+    # O(n), O(n)
     def reverseOnlyLetters(self, s: str) -> str:
-        left, right = 0, len(s) - 1
         s = list(s)
-        while left < right:
-            if s[left].isalpha() and s[right].isalpha():
-                s[left], s[right] = s[right], s[left]
-                left += 1
-                right -= 1
-            if s[left].isalpha() == False:
-                left += 1
-            if s[right].isalpha() == False:
-                right -= 1
-
-        return "".join(s)
+        i, j = 0, len(s) - 1
+        while i <= j:
+            if s[i].isalpha() and s[j].isalpha():
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            else:
+                if not s[i].isalpha():
+                    i += 1
+                if not s[j].isalpha():
+                    j -= 1
+        
+        return ''.join(s)
