da5068ab03c029a135a592c9a25760e421ad2e4c
Zax S <zacks.shen@gmail.com>
2025-08-09 22:00:56 -0400

    File updated at 8/9/2025, 10:00:55 PM

--- 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
+++ 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
@@ -1,15 +1,13 @@
-# Last updated: 8/8/2025, 12:12:12 PM
+# Last updated: 8/9/2025, 10:00:55 PM
 class Solution:
     # O(n), O(1)
     def maxPower(self, s: str) -> int:
         count = power = 1
         for i in range(len(s) - 1):
-            if s[i] == s[i+1]:
+            if s[i] == s[i + 1]:
                 count += 1
                 power = max(power, count)
             else:
-                count = 1
+                count = 1 
         
         return power
-
-
