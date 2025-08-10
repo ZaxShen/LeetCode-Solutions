10fbbc5c176a326506746a547c7c5fc5b74e803e
Zax S <zacks.shen@gmail.com>
2025-08-10 19:33:40 -0400

    File updated at 8/10/2025, 7:33:40 PM

--- 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
+++ 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
@@ -1,12 +1,12 @@
-# Last updated: 8/9/2025, 10:25:18 PM
+# Last updated: 8/10/2025, 7:33:40 PM
 class Solution:
     def maxPower(self, s: str) -> int:
-        count = power = 1
-        for i in range(len(s) - 1): # 0, 1
+        count = max_power = 1
+        for i in range(len(s) - 1):
             if s[i] == s[i + 1]:
                 count += 1
+                max_power = max(max_power, count)
             else:
                 count = 1
-            power = max(power, count)
 
-        return power
+        return max_power
