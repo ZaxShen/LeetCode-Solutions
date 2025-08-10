aa39015737d043c3fe3b6ac250e2ae121fd6db9a
Zax S <zacks.shen@gmail.com>
2025-08-09 22:43:37 -0400

    File updated at 8/9/2025, 10:43:37 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,10 +1,17 @@
-# Last updated: 8/9/2025, 10:40:56 PM
+# Last updated: 8/9/2025, 10:43:37 PM
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         i, j = 0, len(nums) - 1
+
+        # For Full non-positive number
+        if nums[i] >= 0:
+            return [num ** 2 for num in nums]
+        elif nums[j] <= 0:
+            return [num ** 2 for num in nums][::-1]
+
+
         pos = len(nums) - 1
         res = [0] * len(nums)
-
         while i <= j:
             i_sqr, j_sqr = nums[i] ** 2, nums[j] ** 2
             if i_sqr < j_sqr:
