3289ef120087f84d919429adc76eadd3ff298a33
Zax S <zacks.shen@gmail.com>
2025-08-09 22:51:16 -0400

    File updated at 8/9/2025, 10:51:16 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,19 +1,26 @@
-# Last updated: 8/9/2025, 10:49:34 PM
+# Last updated: 8/9/2025, 10:51:16 PM
 class Solution:
-    # O(n), O(1)
+	# O(n), O(1)
     def sortedSquares(self, nums: List[int]) -> List[int]:
         i, j = 0, len(nums) - 1
-        p = len(nums) - 1
-        res = [0] * len(nums)
 
+		# Edge Cases Handling
+        if nums[i] >= 0:
+            return [num ** 2 for num in nums]
+        elif nums[j] <= 0:
+            return [num ** 2 for num in nums][::-1]
+
+        pos = len(nums) - 1
+        res = [0] * len(nums)
+        
         while i <= j:
             i_sq, j_sq = nums[i] ** 2, nums[j] ** 2
             if i_sq < j_sq:
-                res[p] = j_sq
+                res[pos] = j_sq
                 j -= 1
             else:
-                res[p] = i_sq
+                res[pos] = i_sq
                 i += 1
-            p -= 1
-
+            pos -= 1
+        
         return res
