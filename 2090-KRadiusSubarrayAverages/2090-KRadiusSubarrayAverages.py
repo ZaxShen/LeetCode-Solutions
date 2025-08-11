6dbb32517885e01d1d0f7df5c479adcf90eb9ad2
Zax S <zacks.shen@gmail.com>
2025-08-10 23:36:12 -0400

    Create 2090-KRadiusSubarrayAverages

---
+++ 2090-KRadiusSubarrayAverages/2090-KRadiusSubarrayAverages.py
@@ -0,0 +1,25 @@
+# Last updated: 8/10/2025, 11:36:11 PM
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        curr, prefix_sum = 0, []
+        
+        for i in nums:
+            curr += i
+            prefix_sum.append(curr)
+        
+        res = []
+        n = 2 * k + 1
+        edge = len(prefix_sum) - k
+        
+        for i in range(len(prefix_sum)):
+            left = i - k
+            right = i + k
+            if 0 <= left and right < len(nums):
+                ans = (prefix_sum[right] - prefix_sum[left] + nums[left]) // n
+                res.append(ans)
+            else:
+                res.append(-1)
+
+        return res
+
+        
