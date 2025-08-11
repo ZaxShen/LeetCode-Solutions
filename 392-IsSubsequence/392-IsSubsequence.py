e7aa919d9e8cf20f607b3b67ea2b67bbbc687c3d
Zax S <zacks.shen@gmail.com>
2025-08-11 10:59:30 -0400

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.py
@@ -0,0 +1,12 @@
+# Last updated: 8/11/2025, 10:59:30 AM
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        return i == len(s)
