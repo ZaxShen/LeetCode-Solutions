ccb58e6c168349a8629a442d2202a4336dfb60cb
Zax S <zacks.shen@gmail.com>
2025-08-10 21:26:06 -0400

    File updated at 8/10/2025, 9:26:05 PM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
@@ -1,10 +1,7 @@
-# Last updated: 8/10/2025, 9:23:58 PM
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        prefix_sum = []
-        curr = 0
-        for num in nums:
-            curr += num
-            prefix_sum.append(curr)
+# Last updated: 8/10/2025, 9:26:05 PM
+from itertools import accumulate
 
-        return prefix_sum
+class Solution:
+    # O(n), O(1)
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return list(accumulate(nums))
