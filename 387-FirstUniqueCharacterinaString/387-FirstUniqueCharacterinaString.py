1d7c0f233c459659da0b37ee6d1cb279038dddb1
Zax S <zacks.shen@gmail.com>
2025-08-11 00:18:01 -0400

    File updated at 8/11/2025, 12:18:01 AM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
@@ -1,13 +1,10 @@
-# Last updated: 8/11/2025, 12:10:28 AM
+# Last updated: 8/11/2025, 12:18:01 AM
+from collections import Counter
+
 class Solution:
     # O(n), O(n)
     def firstUniqChar(self, s: str) -> int:
-        d = {}
-        for char in s:
-            d[char] = d.get(char, 0) + 1
-        
-        for k, v in d.items():
-            if v == 1:
-                return s.index(k)
+        counter = Counter(s)
+        min_count = min(counter, key=counter.get)
 
-        return -1
+        return s.index(min_count) if counter[min_count] == 1 else -1
