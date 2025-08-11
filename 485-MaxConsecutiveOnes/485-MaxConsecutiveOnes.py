3c2f195368a3795097ad6ef79d8b6e451ae01c30
Zax S <zacks.shen@gmail.com>
2025-08-11 11:15:31 -0400

    File updated at 8/11/2025, 11:15:31 AM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.py
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.py
@@ -1,9 +1,8 @@
-# Last updated: 8/10/2025, 2:31:52 PM
+# Last updated: 8/11/2025, 11:15:31 AM
 class Solution:
-    # O(n), O(1)
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
         max_len = curr_len = 0
-        
+
         for num in nums:
             if num == 1:
                 curr_len += 1
