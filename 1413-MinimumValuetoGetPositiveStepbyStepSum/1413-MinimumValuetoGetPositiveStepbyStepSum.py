a9709534c70a9406f5b606bce33f3d12b57224b4
Zax S <zacks.shen@gmail.com>
2025-08-11 12:23:36 -0400

    File updated at 8/11/2025, 12:23:35 PM

--- 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.py
+++ 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.py
@@ -1,10 +1,11 @@
-# Last updated: 8/10/2025, 10:38:47 PM
+# Last updated: 8/11/2025, 12:23:35 PM
 class Solution:
     # O(n), O(1)
     def minStartValue(self, nums: List[int]) -> int:
-        curr = min_acc = 0
+        curr = min_start_val = 0
+
         for num in nums:
             curr += num
-            min_acc = min(min_acc, curr)
+            min_start_val = min(min_start_val, curr)
 
-        return -min_acc + 1
+        return -min_start_val + 1
