59ae4775bc72f3eeb48bc23c4d249c6e2c968d30
Zax S <zacks.shen@gmail.com>
2025-08-11 12:06:42 -0400

    File updated at 8/11/2025, 12:06:42 PM

--- 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.py
+++ 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.py
@@ -1,14 +1,13 @@
-# Last updated: 8/10/2025, 10:07:35 PM
+# Last updated: 8/11/2025, 12:06:42 PM
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        left_sec = res = 0
+        left_sec = count = 0
         total = sum(nums)
 
         for i in range(len(nums) - 1):
             left_sec += nums[i]
             right_sec = total - left_sec
             if left_sec >= right_sec:
-                res += 1
+                count += 1
 
-        return res
-        
+        return count
