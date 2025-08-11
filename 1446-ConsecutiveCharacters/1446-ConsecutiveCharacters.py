0e4feaffd4b6f9849e9904978cbc82835bcbd7ad
Zax S <zacks.shen@gmail.com>
2025-08-10 20:05:32 -0400

    File updated at 8/10/2025, 8:05:32 PM

--- 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
+++ 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
@@ -1,12 +1,12 @@
-# Last updated: 8/10/2025, 8:00:02 PM
+# Last updated: 8/10/2025, 8:05:32 PM
 class Solution:
     def maxPower(self, s: str) -> int:
         left = max_power = 0
         while left < len(s):
             right = left
             while right < len(s) and s[right] == s[left]:
-                max_power = max(max_power, right - left + 1)
                 right += 1
+            max_power = max(max_power, right - left)
             left = right
 
         return max_power
