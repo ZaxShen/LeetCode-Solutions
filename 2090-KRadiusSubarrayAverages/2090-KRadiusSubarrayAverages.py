3d07cd76725dc55c2da6a750cbdde9d2d16a5bf8
Zax S <zacks.shen@gmail.com>
2025-08-11 12:36:04 -0400

    File updated at 8/11/2025, 12:36:04 PM

--- 2090-KRadiusSubarrayAverages/2090-KRadiusSubarrayAverages.py
+++ 2090-KRadiusSubarrayAverages/2090-KRadiusSubarrayAverages.py
@@ -1,22 +1,18 @@
-# Last updated: 8/10/2025, 11:37:03 PM
+# Last updated: 8/11/2025, 12:36:04 PM
+from itertools import accumulate
+
 class Solution:
-    # O(n), O(n)
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        curr, prefix_sum = 0, []
-
-        for i in nums:
-            curr += i
-            prefix_sum.append(curr)
-
+        prefix_sum = list(accumulate(nums))
         res = []
         n = 2 * k + 1
 
-        for i in range(len(prefix_sum)):
+        for i in range(len(nums)):
             left = i - k
             right = i + k
             if 0 <= left and right < len(nums):
-                ans = (prefix_sum[right] - prefix_sum[left] + nums[left]) // n
-                res.append(ans)
+                _ = prefix_sum[right] - prefix_sum[left] + nums[left]
+                res.append(_ // n)
             else:
                 res.append(-1)
 
