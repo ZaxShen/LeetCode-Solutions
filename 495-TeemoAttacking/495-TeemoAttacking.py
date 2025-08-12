4df631769205a906c3667a8456d156a80425847d
Zax S <zacks.shen@gmail.com>
2025-08-12 19:21:21 -0400

    File updated at 8/12/2025, 7:21:21 PM

--- 495-TeemoAttacking/495-TeemoAttacking.py
+++ 495-TeemoAttacking/495-TeemoAttacking.py
@@ -1,11 +1,8 @@
-# Last updated: 8/12/2025, 7:19:42 PM
+# Last updated: 8/12/2025, 7:21:21 PM
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
         res = 0
         for i in range(len(timeSeries) - 1):
-            if timeSeries[i] + duration < timeSeries[i + 1]:
-                res += duration
-            else:
-                res += timeSeries[i + 1] - timeSeries[i]
+            res += min(duration, timeSeries[i + 1] - timeSeries[i])
 
         return res + duration
