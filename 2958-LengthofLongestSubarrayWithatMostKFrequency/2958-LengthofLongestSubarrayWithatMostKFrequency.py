3e1db65e161f898502a7258d1e190a1b36aa701f
Zax S <zacks.shen@gmail.com>
2025-08-12 19:39:07 -0400

    File updated at 8/12/2025, 7:39:07 PM

--- 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.py
+++ 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.py
@@ -1,18 +1,17 @@
-# Last updated: 8/11/2025, 9:24:58 PM
-from collections import defaultdict, deque
+# Last updated: 8/12/2025, 7:39:07 PM
+from collections import Counter
 
 class Solution:
-	# O(n), O(n)
-    def maxSubarrayLength(self, nums: list[int], k: int) -> int:
-        hashmap = defaultdict(deque)
-        left = max_len = 0
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        counter = Counter()
+        left = 0
+        max_len = 0
 
         for right, num in enumerate(nums):
-            hashmap[num].append(right)
-            if len(hashmap[num]) > k:
-                # move left to next index of the 1st occurance
-                # or prevent from left move back again
-                left = max(left, hashmap[num].popleft() + 1)
+            counter[num] += 1
+            while counter[num] > k:
+                counter[nums[left]] -= 1
+                left += 1
             max_len = max(max_len, right - left + 1)
 
         return max_len
