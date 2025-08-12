d9ebdd4b53ebe0baca6ff6f53be6a3e3288d7e9b
Zax S <zacks.shen@gmail.com>
2025-08-11 20:01:43 -0400

    File updated at 8/11/2025, 8:01:42 PM

--- 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
@@ -1,7 +1,13 @@
-# Last updated: 8/11/2025, 7:56:05 PM
+# Last updated: 8/11/2025, 8:01:42 PM
 from itertools import accumulate
 
 class Solution:
+    # O(n), O(1)
     def largestAltitude(self, gain: List[int]) -> int:
-        res = max(accumulate(gain))
-        return res if res > 0 else 0
+        max_alt = curr_alt = 0
+
+        for point in gain:
+            curr_alt += point
+            max_alt = max(max_alt, curr_alt)
+
+        return max_alt
