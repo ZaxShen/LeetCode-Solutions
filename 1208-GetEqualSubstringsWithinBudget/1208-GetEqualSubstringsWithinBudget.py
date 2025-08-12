5dbfbf5f7185bf9ba29973e125c9066ea2fe2040
Zax S <zacks.shen@gmail.com>
2025-08-12 18:19:54 -0400

    File updated at 8/12/2025, 6:19:54 PM

--- 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.py
+++ 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.py
@@ -1,14 +1,12 @@
-# Last updated: 8/11/2025, 7:27:08 PM
+# Last updated: 8/12/2025, 6:19:54 PM
 class Solution:
-    # O(n), O(1)
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        curr_cost = max_len = left = 0
+        cost = left = meax_len = 0
 
         for right in range(len(s)):
-            curr_cost += abs(ord(s[right]) - ord(t[right]))
-            if curr_cost > maxCost:
-                curr_cost -= abs(ord(s[left]) - ord(t[left]))
+            cost += abs(ord(s[right]) - ord(t[right]))
+            if cost > maxCost:
+                cost -= abs(ord(s[left]) - ord(t[left]))
                 left += 1
-            max_len = max(max_len, right - left + 1)
 
-        return max_len
+        return right - left + 1
