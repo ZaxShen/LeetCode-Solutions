435e09323ce371dd3162c8943a19de4265f473ed
Zax S <zacks.shen@gmail.com>
2025-08-13 09:48:06 -0400

    File updated at 8/13/2025, 9:48:06 AM

--- 2000-ReversePrefixofWord/2000-ReversePrefixofWord.py
+++ 2000-ReversePrefixofWord/2000-ReversePrefixofWord.py
@@ -1,16 +1,9 @@
-# Last updated: 8/11/2025, 6:22:59 PM
+# Last updated: 8/13/2025, 9:48:06 AM
 class Solution:
-    # O(n), O(1)
     def reversePrefix(self, word: str, ch: str) -> str:
-        word = list(word)
-        left = 0
+        try:
+            index = word.index(ch)
+        except ValueError:
+            return word
 
-        for right in range(len(word)):
-            if word[right] == ch:
-                while left < right:
-                    word[left], word[right] = word[right], word[left]
-                    left += 1
-                    right -= 1
-                return ''.join(word)
-
-        return ''.join(word)
+        return word[:index + 1][::-1] + word[index + 1:]
