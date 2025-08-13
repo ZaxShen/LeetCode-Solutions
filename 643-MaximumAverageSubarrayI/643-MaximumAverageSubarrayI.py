bb40e23e29a9840cca39b36c36d0677b3935240c
Zax S <zacks.shen@gmail.com>
2025-08-13 19:35:12 -0400

    File updated at 8/13/2025, 7:35:12 PM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
@@ -1,8 +1,13 @@
-# Last updated: 8/13/2025, 2:12:05 PM
+# Last updated: 8/13/2025, 7:35:12 PM
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        curr = res = sum(nums[:k])
+        # if k <= 1:
+        #     return nums[0]
 
+        # First window
+        res = curr = sum(nums[:k])
+
+        # Sliding window
         for i in range(k, len(nums)):
             curr += nums[i] - nums[i - k]
             res = max(res, curr)
