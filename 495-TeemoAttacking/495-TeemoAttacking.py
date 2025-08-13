958415e16d5055f24a10606f2f34634c9e5a66ef
Zax S <zacks.shen@gmail.com>
2025-08-13 01:15:25 -0400

    File updated at 8/13/2025, 1:15:24 AM

--- 495-TeemoAttacking/495-TeemoAttacking.py
+++ 495-TeemoAttacking/495-TeemoAttacking.py
@@ -1,7 +1,8 @@
-# Last updated: 8/12/2025, 7:21:21 PM
+# Last updated: 8/13/2025, 1:15:24 AM
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
         res = 0
+        
         for i in range(len(timeSeries) - 1):
             res += min(duration, timeSeries[i + 1] - timeSeries[i])
 
