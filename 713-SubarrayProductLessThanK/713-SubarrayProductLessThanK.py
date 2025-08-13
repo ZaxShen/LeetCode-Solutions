b1742ba9800c4790c157b45c4e9ba714a0f8f0bb
Zax S <zacks.shen@gmail.com>
2025-08-13 13:18:10 -0400

    File updated at 8/13/2025, 1:18:10 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.py
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.py
@@ -1,16 +1,19 @@
-# Last updated: 8/10/2025, 3:10:58 PM
+# Last updated: 8/13/2025, 1:18:10 PM
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        curr = 1
+        left = 0
+        res = 0
+
         if k <= 1:
             return 0
-        
-        product = 1
-        left = count = 0
-        for right, v in enumerate(nums):
-            product *= v
-            while product >= k:
-                product /= nums[left]
-                left += 1
-            count += right - left + 1
 
-        return count
+        for right, num in enumerate(nums):
+            curr *= num
+            while curr >= k:
+                curr /= nums[left]
+                left += 1
+            res += right - left + 1
+
+        return res
+
