f664315afce2f52bd0d071fa2bf1c2531ebabe8d
Zax S <zacks.shen@gmail.com>
2025-08-13 01:49:08 -0400

    File updated at 8/13/2025, 1:49:08 AM

--- 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.py
+++ 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.py
@@ -1,11 +1,10 @@
-# Last updated: 8/13/2025, 1:01:04 AM
+# Last updated: 8/13/2025, 1:49:08 AM
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        curr = 0
-        res = float("inf")
+        curr = res = 0
 
         for num in nums:
             curr += num
             res = min(res, curr)
 
-        return max(-res + 1, 1)
+        return -res + 1
