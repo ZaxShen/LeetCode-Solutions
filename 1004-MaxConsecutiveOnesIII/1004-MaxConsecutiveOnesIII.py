1d297a248082c0f86a3dfc0a622abcf64a0c1742
Zax S <zacks.shen@gmail.com>
2025-08-14 13:04:21 -0400

    File updated at 8/14/2025, 1:04:21 PM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.py
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.py
@@ -1,13 +1,13 @@
-# Last updated: 8/13/2025, 1:42:21 AM
+# Last updated: 8/14/2025, 1:04:21 PM
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         left = max_len = 0
 
-        for right, num in enumerate(nums):
-            if num == 0:
+        for right in range(len(nums)):
+            if nums[right] == 0:
                 k -= 1
 
-            while k < 0:
+            if k < 0:
                 if nums[left] == 0:
                     k += 1
                 left += 1
