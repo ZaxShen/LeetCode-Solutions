e15a4ee87e8277ca8509e76295b52c40e8a37d78
Zax S <zacks.shen@gmail.com>
2025-08-14 15:19:42 -0400

    File updated at 8/14/2025, 3:19:41 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,14 +1,14 @@
-# Last updated: 8/13/2025, 1:13:13 AM
+# Last updated: 8/14/2025, 3:19:41 PM
 class Solution:
-    # O()
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        min_len = min(len(s) for s in strs)
+        min_len = min(len(word) for word in strs)
+        res = []
 
         for i in range(min_len):
             char = strs[0][i]
+            for j in strs[1:]:
+                if j[i] != char:
+                    return j[:i]
+            res.append(char)
 
-            for s in strs[1:]:
-                if char != s[i]:
-                    return s[:i]
-
-        return strs[0][:min_len]
+        return ''.join(res)
