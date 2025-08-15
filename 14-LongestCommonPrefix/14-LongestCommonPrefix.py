aa8aaa5e217229e1691b11306f102814dee34cbe
Zax S <zacks.shen@gmail.com>
2025-08-15 17:14:05 -0400

    File updated at 8/15/2025, 5:14:05 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.py
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -1,17 +1,12 @@
-# Last updated: 8/14/2025, 11:39:50 PM
+# Last updated: 8/15/2025, 5:14:05 PM
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         min_len = min(len(s) for s in strs)
-        count = 0
 
         for i in range(min_len):
             char = strs[0][i]
-
             for s in strs[1:]:
                 if s[i] != char:
                     return s[:i]
 
         return strs[0][:min_len]
-
-
-
