d1821983cbdf412122716bd11a1463da3f51b4a8
Zax S <zacks.shen@gmail.com>
2025-08-15 16:32:39 -0400

    File updated at 8/15/2025, 4:32:39 PM

--- 724-FindPivotIndex/724-FindPivotIndex.py
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -1,8 +1,9 @@
-# Last updated: 8/13/2025, 12:54:15 AM
+# Last updated: 8/15/2025, 4:32:39 PM
 class Solution:
+    # O(n), O(1)
     def pivotIndex(self, nums: List[int]) -> int:
         total = sum(nums)
-        left_sec = 0
+        left_sec = res = 0
 
         for i, num in enumerate(nums):
             right_sec = total - left_sec - num
