c7bc56209e8ffeb782dfa2935e0ca4ec82d9ed07
Zax S <zacks.shen@gmail.com>
2025-08-15 18:06:27 -0400

    File updated at 8/15/2025, 6:06:27 PM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,13 +1,8 @@
-# Last updated: 8/15/2025, 5:55:40 PM
-class Solution:
-    # O(n), O(1)
-    def missingNumber(self, nums: List[int]) -> int:
-        # Expected nums: range(len(nums) + 1)
-        n = len(nums) + 1
-        # So max num of nums should be
-        max_num = n - 1
-        # According to Gauss' Formula
-        expected_sum = int(max_num * n / 2)
-        actual_sum = sum(nums)
-
-        return expected_sum - actual_sum
+# Last updated: 8/15/2025, 6:06:27 PM
+    int sum = 0;
+    for (int i = 0; i < nums.length; ++i) {
+        sum += nums[i];
+    }
+    
+    return nums.length * (nums.length + 1) / 2 - sum;
+}
