7a1a3ed50711216a1f770cc3cf6ab279720750fe
Zax S <zacks.shen@gmail.com>
2025-08-16 12:47:13 -0400

    File updated at 8/16/2025, 12:47:12 PM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,6 +1,9 @@
-# Last updated: 8/16/2025, 12:45:46 PM
+# Last updated: 8/16/2025, 12:47:12 PM
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums) + 1
-        expected_nums = range(n)
-        return sum(expected_nums) - sum(nums)
+        # expected_nums = range(n)
+        max_num = n - 1
+        expected_sum = max_num * n // 2
+        
+        return expected_sum - sum(nums)
