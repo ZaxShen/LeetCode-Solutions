6653bef64613965c81ec3d4a8f2c7af5b916cc14
Zax S <zacks.shen@gmail.com>
2025-08-15 20:29:59 -0400

    File updated at 8/15/2025, 8:29:59 PM

--- 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.py
+++ 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.py
@@ -1,15 +1,21 @@
-# Last updated: 8/14/2025, 12:07:51 PM
+# Last updated: 8/15/2025, 8:29:59 PM
 from collections import defaultdict
+
 
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        prefix = defaultdict(int)
-        prefix[0] = 1
-        curr = count = 0
+        seen = defaultdict(int)
+        seen[0] = 1  # Handle empty prefix
 
+        prefix_sum = res = 0
         for num in nums:
-            curr += num % 2
-            count += prefix[curr - k]
-            prefix[curr] += 1
+            prefix_sum += num % 2  # Key transformation
 
-        return count
+            # Look for: prefix_sum - target
+            if prefix_sum - k in seen:
+                # Found! Update result
+                res += seen[prefix_sum - k]
+            # Update seen
+            seen[prefix_sum] += 1
+
+        return res
