fe92417130301bb37fd86d0efb428b6d068622e8
Zax S <zacks.shen@gmail.com>
2025-08-15 23:14:20 -0400

    File updated at 8/15/2025, 11:14:20 PM

--- 525-ContiguousArray/525-ContiguousArray.py
+++ 525-ContiguousArray/525-ContiguousArray.py
@@ -1,21 +1,19 @@
-# Last updated: 8/15/2025, 9:04:34 PM
-from collections import defaultdict
-
+# Last updated: 8/15/2025, 11:14:20 PM
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        seen = defaultdict(int)
-        seen[0] = -1  # Handle empty prefix
+        # Define seen and handle empty 
+        seen = {0: -1}
 
         prefix_sum = res = 0
         for i, num in enumerate(nums):
+            # Update prefix_sum
             prefix_sum += 1 if num == 1 else -1
 
-            # Look for: prefix_sum - target
+            # Looking for a valid prefix_sum
             if prefix_sum in seen:
-                # Found! Update result
                 res = max(res, i - seen[prefix_sum])
-            # Update seen
             else:
+                # Update seen
                 seen[prefix_sum] = i
-            
+
         return res
