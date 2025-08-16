f1ea37718214a36da078f2d0f0b630eee1675b90
Zax S <zacks.shen@gmail.com>
2025-08-16 12:41:49 -0400

    File updated at 8/16/2025, 12:41:49 PM

--- 2351-FirstLettertoAppearTwice/2351-FirstLettertoAppearTwice.py
+++ 2351-FirstLettertoAppearTwice/2351-FirstLettertoAppearTwice.py
@@ -1,18 +1,13 @@
-# Last updated: 8/16/2025, 12:40:50 PM
+# Last updated: 8/16/2025, 12:41:49 PM
 from collections import Counter
 
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
         counter = Counter()
 
-        for i in range(len(s) - 1):
-            if s[i] == s[i + 1]:
-                return s[i]
-            else:
-                if counter[s[i]] == 1:
-                    return s[i]
-            counter[s[i]] += 1
-
-        return s[-1]
+        for char in s:
+            counter[char] += 1
+            if counter[char] == 2:
+                return char
 
         
