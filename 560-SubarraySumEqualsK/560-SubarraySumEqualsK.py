9de91d14f2d33084009efd88f2509f79b4eeff00
Zax S <zacks.shen@gmail.com>
2025-08-15 20:14:56 -0400

    File updated at 8/15/2025, 8:14:56 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,8 +1,9 @@
-# Last updated: 8/15/2025, 8:12:08 PM
+# Last updated: 8/15/2025, 8:14:56 PM
 from collections import defaultdict
 
+
 class Solution:
-	# O(n), O(n)
+    # O(n), O(n)
     def subarraySum(self, nums: List[int], k: int) -> int:
         seen = defaultdict(int)
         # Initialize with 0:1 to handle the case when prefix_sum equals k
@@ -12,7 +13,8 @@ class Solution:
         for num in nums:
             prefix_sum += num
             # Find the number of subarrays ending here with sum k
-            res += seen[prefix_sum - k]
+            if prefix_sum - k in seen:
+                res += seen[prefix_sum - k]
             # Record the current sum into the map
             seen[prefix_sum] += 1
 
