a131fe9f3752e48e27e23bfa98b014b688b476f8
Zax S <zacks.shen@gmail.com>
2025-08-17 17:23:22 -0400

    File updated at 8/17/2025, 5:23:22 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,24 +1,18 @@
-# Last updated: 8/11/2025, 11:09:10 AM
+# Last updated: 8/17/2025, 5:23:22 PM
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         left, right = 0, len(nums) - 1
-        pos = right
         res = [0] * len(nums)
-
-        # Handle edge cases for better performance
-        if nums[left] >= 0:
-            return [num ** 2 for num in nums]
-        if nums[right] <= 0:
-            return [num ** 2 for num in nums][::-1]
+        pos = right
 
         while left <= right:
-            left_sq, right_sq = nums[left] ** 2, nums[right] ** 2
-            if left_sq < right_sq:
-                res[pos] = right_sq
+            sq_left, sq_right = nums[left] ** 2, nums[right] ** 2
+            if sq_left < sq_right:
+                res[pos] = sq_right
                 right -= 1
             else:
-                res[pos] = left_sq
+                res[pos] = sq_left
                 left += 1
             pos -= 1
-
+        
         return res
