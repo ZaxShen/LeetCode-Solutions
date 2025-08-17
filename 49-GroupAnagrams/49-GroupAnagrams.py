982346a17709dea166881edbda99fed87daac3cb
Zax S <zacks.shen@gmail.com>
2025-08-17 16:26:52 -0400

    File updated at 8/17/2025, 4:26:52 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,11 +1,13 @@
-# Last updated: 8/17/2025, 4:21:09 PM
+# Last updated: 8/17/2025, 4:26:52 PM
 from collections import defaultdict
 
 class Solution:
+    # O(n*m*mlogm), O(n*m)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
 
         for s in strs:
-            hashmap[''.join(sorted(s))].append(s)
+            key = ''.join(sorted(s))
+            hashmap[key].append(s)
 
         return list(hashmap.values())
