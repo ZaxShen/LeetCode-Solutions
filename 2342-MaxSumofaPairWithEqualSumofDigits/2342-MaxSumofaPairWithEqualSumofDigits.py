4fb081639f710fe4bfe7169419d8d67d13afd464
Zax S <zacks.shen@gmail.com>
2025-08-17 19:22:32 -0400

    File updated at 8/17/2025, 7:22:31 PM

--- 2342-MaxSumofaPairWithEqualSumofDigits/2342-MaxSumofaPairWithEqualSumofDigits.py
+++ 2342-MaxSumofaPairWithEqualSumofDigits/2342-MaxSumofaPairWithEqualSumofDigits.py
@@ -1,16 +1,18 @@
-# Last updated: 8/17/2025, 7:07:30 PM
-from collections import defaultdict, Counter
+# Last updated: 8/17/2025, 7:22:31 PM
+from collections import defaultdict
 
 class Solution:
-    # O(n*m), O(k)
+    # O(n*logm), O(k)
     def maximumSum(self, nums: List[int]) -> int:
-        seen = defaultdict(int)
+        largest_seen = defaultdict(int)
         max_sum = -1
         
         for num in nums:
-            sum_digits = sum(map(int, str(num)))
-            if sum_digits in seen:
-                max_sum = max(max_sum, seen[sum_digits] + num)
-            seen[sum_digits] = max(num, seen[sum_digits])
-
+            digit_sum = sum(map(int, str(num)))
+            
+            if digit_sum in largest_seen:
+                max_sum = max(max_sum, largest_seen[digit_sum] + num)
+            
+            largest_seen[digit_sum] = max(num, largest_seen[digit_sum])
+        
         return max_sum
