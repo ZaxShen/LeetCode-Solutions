4299a7f7de99c2678f0790606eed030b4ebec4a4
Zax S <zacks.shen@gmail.com>
2025-08-17 18:07:35 -0400

    File updated at 8/17/2025, 6:07:35 PM

--- 2260-MinimumConsecutiveCardstoPickUp/2260-MinimumConsecutiveCardstoPickUp.py
+++ 2260-MinimumConsecutiveCardstoPickUp/2260-MinimumConsecutiveCardstoPickUp.py
@@ -1,14 +1,13 @@
-# Last updated: 8/17/2025, 6:05:48 PM
-from collections import defaultdict
-
+# Last updated: 8/17/2025, 6:07:35 PM
 class Solution:
+    # O(n), O(m)
     def minimumCardPickup(self, cards: List[int]) -> int:
-        card_pos = defaultdict(list)
+        seen ={}
 
         res = float('inf')
         for i, card in enumerate(cards):
-            if card in card_pos:
-                res = min(res, i - card_pos[card][-1] + 1)
-            card_pos[card].append(i)
+            if card in seen:
+                res = min(res, i - seen[card] + 1)
+            seen[card] = i
 
         return res if res != float('inf') else -1
