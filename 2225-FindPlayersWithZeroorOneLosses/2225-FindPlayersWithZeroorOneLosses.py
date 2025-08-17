209c8f05af0b54c906acd37b17731b44f5a8097a
Zax S <zacks.shen@gmail.com>
2025-08-17 00:34:23 -0400

    File updated at 8/17/2025, 12:34:23 AM

--- 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.py
+++ 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.py
@@ -1,17 +1,17 @@
-# Last updated: 8/17/2025, 12:16:55 AM
+# Last updated: 8/17/2025, 12:34:23 AM
 from collections import Counter
 
 class Solution:
     # O(nlogn), O(n)
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        losses_count = Counter()
+        loss_count = Counter()
 
         for winner, loser in matches:
-            losses_count[winner] = losses_count.get(winner, 0)
-            losses_count[loser] += 1
+            loss_count[winner] += 0  # Ensure winner exists with 0 losses
+            loss_count[loser] += 1
 
         zero_loss, one_loss = [], []
-        for player, loss in losses_count.items():
+        for player, loss in loss_count.items():
             if loss == 0:
                 zero_loss.append(player)
             elif loss == 1:
