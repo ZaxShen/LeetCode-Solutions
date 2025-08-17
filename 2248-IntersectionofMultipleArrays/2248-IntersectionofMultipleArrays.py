d519695fd1b8489c85e5a71cdf5caad46aaebe3e
Zax S <zacks.shen@gmail.com>
2025-08-17 12:30:16 -0400

    File updated at 8/17/2025, 12:30:16 PM

--- 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.py
+++ 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.py
@@ -1,4 +1,4 @@
-# Last updated: 8/14/2025, 9:46:40 AM
+# Last updated: 8/17/2025, 12:30:16 PM
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
         res = set(nums[0])
