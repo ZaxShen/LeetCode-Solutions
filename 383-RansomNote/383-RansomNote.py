a44758bb8c29c4ca0061ad8b2d3540b7d5e02a95
Zax S <zacks.shen@gmail.com>
2025-08-17 20:04:48 -0400

    File updated at 8/17/2025, 8:04:48 PM

--- 383-RansomNote/383-RansomNote.py
+++ 383-RansomNote/383-RansomNote.py
@@ -1,18 +1,18 @@
-# Last updated: 8/17/2025, 7:50:56 PM
-from collections import Counter
-
+# Last updated: 8/17/2025, 8:04:48 PM
 class Solution:
-    # O(n), O(n+m)
+    # O(n+m), O(m)
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # Early termination
         if len(ransomNote) > len(magazine):
             return False
 
-        m_counter = Counter(magazine)
+        m_counter = {}
+        for char in magazine:
+            m_counter[char] = m_counter.get(char, 0) + 1
 
         for char in ransomNote:
-            m_counter[char] -= 1
-            if m_counter[char] < 0:
+            if m_counter.get(char, 0) == 0:
                 return False
+            m_counter[char] -= 1
 
         return True
