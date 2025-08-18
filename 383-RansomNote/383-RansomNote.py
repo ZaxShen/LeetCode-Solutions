bee9c47089c77bd36d5d24e361027fc138973a25
Zax S <zacks.shen@gmail.com>
2025-08-18 12:03:28 -0400

    Delete 383-RansomNote directory

--- 383-RansomNote/383-RansomNote.py
+++
@@ -1,7 +0,0 @@
-# Last updated: 8/17/2025, 8:05:14 PM
-from collections import Counter
-
-class Solution:
-    # O(n+m), O(n+m)
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return not (Counter(ransomNote) - Counter(magazine))
