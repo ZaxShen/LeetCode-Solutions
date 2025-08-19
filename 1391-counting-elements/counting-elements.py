d65e1d18bf718ed637274884ec8452d80a89739a
Zax S <zacks.shen@gmail.com>
2025-08-19 11:34:13 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (34.93%) - LeetSync

---
+++ 1391-counting-elements/counting-elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    # O(n), O(k)
+    def countElements(self, arr: List[int]) -> int:
+        hashset = set(arr)
+
+        count = 0
+        for num in arr:
+            if num + 1 in hashset:
+                count += 1
+
+        return count
