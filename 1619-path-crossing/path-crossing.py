335070ce8695fac530406ec14a1bcf8e35d557f6
Zax S <zacks.shen@gmail.com>
2025-08-19 10:05:20 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (6.68%) - LeetSync

--- 1619-path-crossing/path-crossing.py
+++ 1619-path-crossing/path-crossing.py
@@ -1,13 +1,17 @@
 class Solution:
-    # O(n), O(n)
     def isPathCrossing(self, path: str) -> bool:
-        x = y = 0
         visited = {(0, 0)}
-        
-        for d in path:
-            x += (d == 'E') - (d == 'W')
-            y += (d == 'N') - (d == 'S')
+        x = y = 0
+
+        for move in path:
+            match move:
+                case 'N': y += 1
+                case 'S': y -= 1
+                case 'E': x += 1
+                case 'W': x -= 1
+
             if (x, y) in visited:
                 return True
             visited.add((x, y))
+
         return False
