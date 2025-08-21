4ebdc681ff069f8c694670f6b9e7c01d38bd0264
Zax S <zacks.shen@gmail.com>
2025-08-21 15:37:56 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.25%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,5 +1,17 @@
 from collections import Counter
 
 class Solution:
+    # O(n), O(1)
     def customSortString(self, order: str, s: str) -> str:
-        return ''.join(sorted(s, key=lambda x: order.find(x) if x in order else len(order)))
+        count = Counter(s)
+        res = []
+
+        for char in order:
+            if char in count:
+                res.append(char * count[char])
+                del count[char]
+
+        for char, freq in count.items():
+            res.append(char * freq)
+
+        return ''.join(res)
