8e9dbbaa0519aa397bffb597c8bf5b22197a85a0
Zax S <zacks.shen@gmail.com>
2025-08-21 14:55:14 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.71%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+
+        if len(pattern) != len(s):
+            return False
+
+        return tuple(map(pattern.index, pattern)) == tuple(map(s.index, s))
