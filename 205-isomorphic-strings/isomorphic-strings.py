d6675ea4194557e06b1bfb0e53bd0cc1d7e3827e
Zax S <zacks.shen@gmail.com>
2025-08-21 14:49:16 -0400

    Time: 7 ms (54.05%) | Memory: 19.6 MB (7.60%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,4 +1,13 @@
+from collections import defaultdict
+
 class Solution:
     # O(n), O(n)
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return tuple(map(s.index, s)) == tuple(map(t.index, t))
+        hash_s = defaultdict(list)
+        hash_t = defaultdict(list)
+
+        for i in range(len(s)):
+            hash_s[s[i]].append(i)
+            hash_t[t[i]].append(i)
+
+        return tuple(hash_s.values()) == tuple(hash_t.values())
