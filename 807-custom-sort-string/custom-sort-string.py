bfc7304bd4ce4200e1a8539ae97cbcc4e559864d
Zax S <zacks.shen@gmail.com>
2025-08-21 15:32:35 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (87.95%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.py
@@ -0,0 +1,5 @@
+from collections import Counter
+
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        return ''.join(sorted(s, key=lambda x: order.find(x) if x in order else len(order)))
