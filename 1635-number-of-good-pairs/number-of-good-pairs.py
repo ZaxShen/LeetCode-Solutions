14e1ce37cb285ea20c9a2a11496013ef50e97aaf
Zax S <zacks.shen@gmail.com>
2025-08-24 14:02:09 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.11%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
 
 class Solution:
-	# O(n), O(k)
-    def numIdenticalPairs(self, nums: list[int]) -> int:
-        count = defaultdict(int)
-        res = 0
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        d = defaultdict(int)
 
+        res = 0
         for num in nums:
-            res += count[num]
-            count[num] += 1
-        
+            res += d[num]
+            d[num] += 1
+
         return res
