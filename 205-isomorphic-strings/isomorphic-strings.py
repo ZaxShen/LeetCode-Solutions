17cb79eee169079c9d0567a48debf0472d85d536
Zax S <zacks.shen@gmail.com>
2025-08-26 23:50:38 -0400

    Time: 10 ms (17.31%) | Memory: 18.3 MB (7.58%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,12 +1,3 @@
-from collections import defaultdict
-
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        hash_s = defaultdict(list)
-        hash_t = defaultdict(list)
-
-        for i in range(len(s)):
-            hash_s[s[i]].append(i)
-            hash_t[t[i]].append(i)
-
-        return tuple(hash_s.values()) == tuple(hash_t.values())
+        return tuple(map(s.index, s)) == tuple(map(t.index, t))
