a7757ca354833f2a4abf26c9199efb88a2586733
Zax S <zacks.shen@gmail.com>
2025-09-01 10:22:44 -0400

    Time: 1 ms (35.41%) | Memory: 17.8 MB (81.50%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {"{": "}", "[": "]", "(": ")"}
+        stack = []
+
+        for char in s:
+            if stack and mapping.get(stack[-1]) == char:
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
