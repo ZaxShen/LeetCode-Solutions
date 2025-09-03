1b6e473a3bfbe36a36e57bfc9b359319cdb840b3
Zax S <zacks.shen@gmail.com>
2025-09-03 12:19:47 -0400

    Time: 25 ms (88.99%) | Memory: 20.5 MB (38.55%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,17 +1,14 @@
-from collections import Counter
-
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        seen = Counter()
-        seen[0] = 1
+        seen = {0: 1}
         prefix = count = 0
 
         for i, num in enumerate(nums):
             prefix += num
             lookup = prefix - k
 
-            count += seen[lookup]
+            count += seen.get(lookup, 0)
 
-            seen[prefix] += 1
+            seen[prefix] = seen.get(prefix, 0) + 1
 
         return count
