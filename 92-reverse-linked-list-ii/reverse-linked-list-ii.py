59ed3b8a33515e767334e55c1d16ac97c68dd5b2
Zax S <zacks.shen@gmail.com>
2025-09-03 11:52:01 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.23%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -1,19 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
+    # O(n), O(1)
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # Create dummy node to simplify edge cases
         dummy = ListNode(0, head)
+        
+        # Find the node just before the reversal section
         prev = dummy
         for _ in range(left - 1):
             prev = prev.next
+        
+        # Identify key node
+        start = prev.next  # First node to reverse
 
-        curr = prev.next
+        # Reverse the sublist using "move to front" technique
         for _ in range(right - left):
-            tmp = curr.next
-
-            tmp.next, prev.next, curr.next = prev.next, tmp, tmp.next
-
+            curr = start.next  # Current node being processed
+            curr.next, prev.next, start.next = prev.next, curr, curr.next
+        
         return dummy.next
