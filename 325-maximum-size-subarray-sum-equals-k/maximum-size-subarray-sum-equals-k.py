0064c8c0be19c47420ff8ae8b10657c68757b1e1
Zax S <zacks.shen@gmail.com>
2025-09-03 21:20:55 -0400

    Time: 128 ms (97.59%) | Memory: 63 MB (94.12%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -1,17 +1,17 @@
 class Solution:
-    # O(n), O(n)
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         seen = {0: -1}
-        prefix = res = 0
+        prefix = max_len = 0
 
         for i, num in enumerate(nums):
             prefix += num
             lookup = prefix - k
 
             if lookup in seen:
-                res = max(res, i - seen[lookup])
+                max_len = max(max_len, i - seen[lookup])
 
+            # Storing the 1st appearance for max length
             if prefix not in seen:
                 seen[prefix] = i
 
-        return res
+        return max_len
