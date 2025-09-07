ec0ccd07bc856b0a957c11bae3b53fa3a6cfd08a
Zax S <zacks.shen@gmail.com>
2025-09-07 02:22:06 -0400

    Time: 47 ms (39.27%) | Memory: 17.8 MB (63.06%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,17 +5,20 @@ class Solution:
         if len(s1) > len(s2):
             return False
 
-        window_size = len(s1)
+        win_size = len(s1)
+
+        # First window
         perm1 = Counter(s1)
-        perm2 = Counter(s2[:window_size])
+        perm2 = Counter(s2[:win_size])
 
         if perm1 == perm2:
             return True
 
-        for i in range(window_size, len(s2)):
+        # Slide window
+        for i in range(win_size, len(s2)):
             perm2[s2[i]] += 1
-            perm2[s2[i - window_size]] -= 1
-            if perm2 == perm1:
+            perm2[s2[i - win_size]] -= 1
+            if perm1 == perm2:
                 return True
 
         return False
