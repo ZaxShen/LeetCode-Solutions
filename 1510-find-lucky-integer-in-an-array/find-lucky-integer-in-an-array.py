328b05942d3b059756b9aee545e637fedd9514a0
Zax S <zacks.shen@gmail.com>
2025-09-18 21:05:49 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (97.15%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,13 +1,12 @@
 from collections import Counter
 
 class Solution:
-    # O(n), O(k)
     def findLucky(self, arr: List[int]) -> int:
         count = Counter(arr)
-
         res = -1
-        for k, v in count.items():
-            if k == v:
-                res = max(res, k)
+
+        for i, v in count.items():
+            if i == v:
+                res = max(res, i)
 
         return res
