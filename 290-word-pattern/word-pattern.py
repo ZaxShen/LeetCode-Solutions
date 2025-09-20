31628c803165641c28ebf2e6355c96d738914ac7
Zax S <zacks.shen@gmail.com>
2025-09-20 13:36:33 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.78%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,17 +1,8 @@
-from collections import defaultdict
-
 class Solution:
-    # O(n), O(n)
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
 
         if len(pattern) != len(s):
             return False
 
-        hash_p = defaultdict(list)
-        hash_s = defaultdict(list)
-        for i in range(len(pattern)):
-            hash_p[pattern[i]].append(i)
-            hash_s[s[i]].append(i)
-
-        return tuple(hash_p.values()) == tuple(hash_s.values())
+        return tuple(map(pattern.index, pattern)) == tuple(map(s.index, s))
