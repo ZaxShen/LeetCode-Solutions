43b11accf6e1bcf4d895e4e61854e755eb694fb5
Zax S <zacks.shen@gmail.com>
2025-09-19 21:12:39 -0400

    Time: 176 ms (87.88%) | Memory: 28.7 MB (82.94%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,21 +1,16 @@
-from collections import Counter
-
-
 class Solution:
-    # O(n), O(n)
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        hashset = set()
+        seen = set()
 
         left = prefix = res = 0
         for num in nums:
             prefix += num
-
-            while num in hashset:
+            while num in seen:
                 prefix -= nums[left]
-                hashset.remove(nums[left])
+                seen.remove(nums[left])
                 left += 1
 
-            hashset.add(num)
             res = max(res, prefix)
+            seen.add(num)
 
         return res
