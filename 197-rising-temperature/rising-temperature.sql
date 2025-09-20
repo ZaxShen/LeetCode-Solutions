ff2ecfb093a3dd8e02a60e8011eabef46a02b504
Zax S <zacks.shen@gmail.com>
2025-09-20 16:24:00 -0700

    Time: 224 ms (72.13%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 select w2.id
 from Weather w1
-inner join Weather w2
+join Weather w2
     on w1.recordDate + 1 = w2.recordDate
-where w1.temperature < w2.temperature
+where w2.temperature > w1.temperature
