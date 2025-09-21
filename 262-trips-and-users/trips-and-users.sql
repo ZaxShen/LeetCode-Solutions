6ed06230a607acd28fb904ae28915ea956ee2997
Zax S <zacks.shen@gmail.com>
2025-09-21 14:08:57 -0700

    Time: 190 ms (72.16%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,11 +1,17 @@
-select
-    request_at as Day,
-    round(count(*) filter (where t.status != 'completed')::DECIMAL / count(*), 2) as "Cancellation Rate"
-from trips t
-join users u1 on t.client_id = u1.users_id and u1.role = 'client' and u1.banned = 'No'
-join users u2 on t.driver_id = u2.users_id and u2.role = 'driver' and u2.banned = 'No'
-where request_at between '2013-10-01' and '2013-10-03'
-group by Day
--- where Day::date between '2013-10-01' and '2013-10-03'
--- WHERE Day BETWEEN '2013-10-01' AND '2013-10-03'
--- WHERE CAST('2013-10-01' AS DATE) <= Day AND Day <= CAST('2013-10-03' AS DATE)
+SELECT
+    request_at AS "Day",
+    ROUND(
+        COUNT(*) FILTER (WHERE status ILIKE 'cancelled%')::DECIMAL / COUNT(*),
+        2
+    ) AS "Cancellation Rate"
+FROM
+    trips t
+    JOIN users client ON t.client_id = client.users_id 
+        AND client.role = 'client' 
+        AND client.banned = 'No'
+    JOIN users driver ON t.driver_id = driver.users_id 
+        AND driver.role = 'driver' 
+        AND driver.banned = 'No'
+WHERE request_at::DATE BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at
+ORDER BY request_at;
