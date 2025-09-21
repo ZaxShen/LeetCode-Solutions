544d7a832238660d5c8f4079088a96601f62c010
Zax S <zacks.shen@gmail.com>
2025-09-21 10:16:40 -0700

    Time: 470 ms (48.98%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,9 +1,6 @@
--- Write your PostgreSQL query statement below
-select Country.name as country
-from Calls
-join Person
-    on Person.id in (Calls.caller_id, Calls.callee_id)
-join Country
-    on SUBSTRING(Person.phone_number, 1, 3) = Country.country_code
-group by Country.name
-having avg(duration) > (select avg(duration) from Calls)
+select country.name as country
+from calls
+join person on person.id in (calls.caller_id, calls.callee_id)
+join country on substring(person.phone_number, 1, 3) = country.country_code
+group by country.name
+having avg(duration) > (select avg(duration) from calls)
