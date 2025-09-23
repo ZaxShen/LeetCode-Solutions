846a301126be71d8ce11b9d3911b10e61d6812f0
Zax S <zacks.shen@gmail.com>
2025-09-23 10:56:07 -0700

    Time: 193 ms (66.32%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,12 +1,14 @@
 select
     t.request_at::date as day,
-    round(count(*) filter (where t.status ~ '^cancelled')::decimal / count(*), 2) as "Cancellation Rate"
+    round(count(*) filter(where t.status like 'cancelled%')::decimal / count(*), 2) as "Cancellation Rate"
 from trips t
-    join users clients on t.client_id = clients.users_id 
-        and clients.role = 'client' 
+    join users clients
+        on t.client_id = clients.users_id
         and clients.banned = 'No'
-    join users drivers on t.driver_id = drivers.users_id 
-        and drivers.role = 'driver' 
+        and clients.role = 'client'
+    join users drivers
+        on t.driver_id = drivers.users_id
         and drivers.banned = 'No'
+        and drivers.role = 'driver'
 where t.request_at::date between '2013-10-01' and '2013-10-03'
-group by t.request_at
+group by t.request_at::date
