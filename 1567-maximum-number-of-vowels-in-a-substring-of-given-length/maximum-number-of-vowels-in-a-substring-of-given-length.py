f353a98ede40c86c4fb158296e8941ce25b8db42
Zax S <zacks.shen@gmail.com>
2025-09-23 15:06:33 -0700

    Time: 75 ms (63.26%) | Memory: 18 MB (55.99%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,19 +1,15 @@
 class Solution:
-    # O(n), O(1)
     def maxVowels(self, s: str, k: int) -> int:
-        vowels = set('aeiou')
-
-        # First window
-        # res = window_count = sum(char in vowels for char in s[:k])
-        res = window_count = sum(map(lambda char: char in vowels, s[:k]))
+        vowels = set("aeiou")
+        # Frist window
+        res = win_cnt = sum(map(lambda char: char in vowels, s[:k]))
 
         # Slide window
         for i in range(k, len(s)):
-            window_count += (s[i] in vowels) - (s[i - k] in vowels)
-            res = max(res, window_count)
+            win_cnt += (s[i] in vowels) - (s[i - k] in vowels)
+            res = max(res, win_cnt)
 
             if res == k:
-                return res
+                break
 
         return res
-            
