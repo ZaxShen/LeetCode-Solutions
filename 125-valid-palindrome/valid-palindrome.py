e4ee0a2b4bda97797dd258f311c1b6abb2c60179
Zax S <zacks.shen@gmail.com>
2025-09-23 12:23:37 -0700

    Time: 11 ms (34.81%) | Memory: 17.9 MB (82.88%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         left, right = 0, len(s) - 1
-
-        while left < right:
-            if not s[left].isalnum():
+        while left <= right:
+            if s[left].isalnum() == False:
                 left += 1
-            elif not s[right].isalnum():
+            elif s[right].isalnum() == False:
                 right -= 1
             else:
                 if s[left].lower() != s[right].lower():
@@ -14,4 +13,3 @@ class Solution:
                 right -= 1
 
         return True
-            
