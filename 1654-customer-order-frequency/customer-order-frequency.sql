02be6e8fa9b23ad72a85f6c0101aa48dab2947cd
Zax S <zacks.shen@gmail.com>
2025-09-22 20:31:22 -0700

    Time: 254 ms (35.90%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -1,31 +1,21 @@
-WITH
-    customer_spend AS (
-        SELECT
-            o.customer_id,
-            c.name,
-            DATE_TRUNC('month', o.order_date) AS month_year,
-            SUM(p.price * o.quantity) AS monthly_spend
-        FROM
-            orders o
-            JOIN customers c USING (customer_id)
-            JOIN product p USING (product_id)
-        WHERE
-            o.order_date >= '2020-06-01'
-            AND o.order_date < '2020-08-01'
-        GROUP BY
-            o.customer_id,
-            c.name,
-            DATE_TRUNC('month', o.order_date)
-        HAVING
-            SUM(p.price * o.quantity) >= 100
-    )
-SELECT
-    customer_id,
-    NAME
-FROM
-    customer_spend
-GROUP BY
-    customer_id,
-    NAME
-HAVING
-    COUNT(*) = 2
+with customer_spend as (
+    select
+        customer_id,
+        name,
+        date_trunc('month', order_date) as year_month,
+        sum(price * quantity) as spend
+    from orders
+    join customers using(customer_id)
+    join product using(product_id)
+    where order_date >= '2020-06-01'
+        and order_date < '2020-08-01'
+    group by
+        customer_id,
+        name,
+        date_trunc('month', order_date)
+)
+select customer_id, name
+from customer_spend
+where spend >= 100
+group by customer_id, name
+    having count(*) = 2 
