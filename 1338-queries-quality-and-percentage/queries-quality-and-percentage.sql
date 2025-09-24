658fa3b104e6e734de972ea82f654b77c275bc0e
Zax S <zacks.shen@gmail.com>
2025-09-24 10:13:09 -0700

    Time: 334 ms (8.46%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 select
     query_name,
-    round(avg(rating::decimal / position), 2) as quality,
-    round(avg((rating < 3)::integer) * 100, 2) as poor_query_percentage
-from queries
+    round(avg((rating::numeric / position)), 2) as quality,
+    round((count(*) filter(where rating < 3))::numeric / count(*) * 100, 2) as poor_query_percentage
+from Queries
 group by query_name
