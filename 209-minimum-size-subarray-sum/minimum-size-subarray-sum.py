e2201e28455dc97428e77d402e4d44e05931e508
Zax S <zacks.shen@gmail.com>
2025-09-24 14:41:28 -0700

    Time: 8 ms (66.72%) | Memory: 28.4 MB (10.80%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        win_sum = 0
         min_len = float('inf')
-        left = win_sum = 0
 
         for right, num in enumerate(nums):
             win_sum += num
@@ -10,5 +11,5 @@ class Solution:
                 min_len = min(min_len, right - left + 1)
                 win_sum -= nums[left]
                 left += 1
-
+            
         return min_len if min_len != float('inf') else 0
