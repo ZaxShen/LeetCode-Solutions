8294263ab81522b079c83f64e5f78d93044126f9
Zax S <zacks.shen@gmail.com>
2025-09-24 10:50:23 -0700

    Time: 883 ms (98.16%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,7 @@
-select contest_id, round(count(*)::DECIMAL / (select count(user_id) from users) * 100, 2) as percentage
+select
+    contest_id,
+    round(count(*)::numeric / (select count(*) from Users) * 100, 2) as percentage
 from register
 group by contest_id
-order by percentage desc, contest_id asc
+order by
+    percentage desc, contest_id asc
