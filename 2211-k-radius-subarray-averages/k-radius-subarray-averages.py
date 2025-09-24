d6df5aaa2b2d70d5f7b4ad63fac887d0122a562d
Zax S <zacks.shen@gmail.com>
2025-09-24 16:59:52 -0700

    Time: 46 ms (96.89%) | Memory: 31.8 MB (99.16%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -1,19 +1,23 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        if k == 0:
-            return nums
         if len(nums) < 2 * k + 1:
             return [-1] * len(nums)
 
-        # First window
+        if k == 0:
+            return nums
+
+        n = len(nums)
+        res = [-1] * n
         win_size = 2 * k + 1
+        # Frist window
         win_sum = sum(nums[:win_size])
-        res = [-1] * len(nums)
-        res[k] = win_sum // win_size
+        win_avg = win_sum // win_size
+        res[k] = win_avg
 
         # Slide window
         for i in range(win_size, len(nums)):
             win_sum += nums[i] - nums[i - win_size]
-            res[i - k] = win_sum // win_size
+            win_avg = win_sum // win_size
+            res[i - k] = win_avg
 
         return res
