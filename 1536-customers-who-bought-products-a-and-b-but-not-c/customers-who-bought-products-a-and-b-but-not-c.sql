34b7da2fc758c0592661eeaa3b4d9eff3e15e5c1
Zax S <zacks.shen@gmail.com>
2025-09-24 09:16:49 -0700

    Time: 431 ms (5.26%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,11 +1,34 @@
+with required_products as (
+    select distinct
+        c.customer_id,
+        c.customer_name,
+        o.product_name
+    from Customers c
+    -- if there is product table, use it for completed grid
+    cross join Orders o
+),
+customer_product_grid AS (
+    select
+        rp.customer_id,
+        rp.customer_name,
+        rp.product_name,
+        count(o.product_name) > 0 as purchased
+    from required_products rp
+    left join Orders o on rp.customer_id = o.customer_id
+        and rp.product_name = o.product_name
+    group by rp.customer_id, rp.customer_name, rp.product_name
+),
+customer_summary as (
 select
-    c.customer_id,
-    c.customer_name
-from Customers c
-join Orders o using(customer_id)
-group by c.customer_id, c.customer_name
-having
-    sum(case when o.product_name = 'A' then 1 else 0 end) > 0
-    and sum(case when o.product_name = 'B' then 1 else 0 end) > 0
-    and sum(case when o.product_name = 'C' then 1 else 0 end) = 0
-order by c.customer_id
+    customer_id,
+    customer_name,
+    bool_and(case when product_name in ('A', 'B') then purchased else true end) as bought_a_and_b,
+    bool_or(case when product_name = 'C' then purchased else false end) as bought_c
+from customer_product_grid
+group by customer_id, customer_name
+order by customer_id
+)
+select customer_id, customer_name
+from customer_summary
+where bought_a_and_b is true
+    and bought_c is false
