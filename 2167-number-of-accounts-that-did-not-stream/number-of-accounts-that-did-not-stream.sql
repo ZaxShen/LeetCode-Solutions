b9275bfc3bde2181ef9c8463b831e3df0f4eaec6
Zax S <zacks.shen@gmail.com>
2025-09-25 10:29:19 -0700

    Time: 291 ms (93.55%) | Memory: 0B (100.00%) - LeetSync

--- 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -1,5 +1,5 @@
 select count(*) as accounts_count
 from Subscriptions su
-left join Streams st using(account_id)
+join Streams st using(account_id)
 where extract(year from su.end_date) = 2021
     and extract(year from st.stream_date) != 2021
