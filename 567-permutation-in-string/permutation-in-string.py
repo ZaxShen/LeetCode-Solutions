c58dca7215a7b4096f877a1d435667b8a5f72405
Zax S <zacks.shen@gmail.com>
2025-09-24 17:50:15 -0700

    Time: 59 ms (28.96%) | Memory: 17.8 MB (62.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,21 +4,17 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        
-        c1 = Counter(s1)
-        c2 = Counter(s2[:len(s1)])  # First window
-        if c1 == c2:
+
+        n = len(s1)
+        perm_s1 = Counter(s1)
+        perm_s2 = Counter(s2[:n])
+        if perm_s2 >= perm_s1:
             return True
 
-        left = 0
-        for right in range(len(s1), len(s2)):
-            c2[s2[right]] += 1
-            c2[s2[left]] -= 1
-            left += 1
-
-            if c1 == c2:
+        for i in range(n, len(s2)):
+            perm_s2[s2[i]] += 1
+            perm_s2[s2[i - n]] -= 1
+            if perm_s2 >= perm_s1:
                 return True
 
         return False
-
-
