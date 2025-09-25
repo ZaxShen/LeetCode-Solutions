b8a4a7623671dde9de2ade5e9366f66cb6b4528c
Zax S <zacks.shen@gmail.com>
2025-09-25 10:56:34 -0700

    Time: 544 ms (6.45%) | Memory: 0B (100.00%) - LeetSync

--- 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -1,11 +1,8 @@
 SELECT
-    COUNT(su.account_id) AS accounts_count
+    COUNT(account_id) AS accounts_count
 FROM
-    subscriptions su
-    LEFT JOIN streams st ON su.account_id = st.account_id 
-        AND EXTRACT(YEAR FROM st.stream_date) = 2021
+    subscriptions
+    JOIN streams USING (account_id)
 WHERE
-    -- Subscription was active during 2021
-    (su.start_date <= '2021-12-31' AND su.end_date >= '2021-01-01')
-    -- No streams in 2021
-    AND st.account_id IS NULL;
+    EXTRACT(YEAR FROM end_date) = 2021
+    AND EXTRACT(YEAR FROM stream_date) != 2021
