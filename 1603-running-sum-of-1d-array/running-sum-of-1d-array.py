13ceef9794d576dc4bc25f6802373113344635b5
Zax S <zacks.shen@gmail.com>
2025-09-25 13:35:47 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (42.72%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,9 +1,5 @@
+from itertools import accumulate
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        res = []
-        prefix = 0
-        for num in nums:
-            prefix += num
-            res.append(prefix)
-
-        return res
+        return list(accumulate(nums))
