66788130bab7677045d941257dd606eb45cce10b
Zax S <zacks.shen@gmail.com>
2025-09-26 10:36:31 -0700

    Time: 188 ms (82.22%) | Memory: 26.3 MB (62.03%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -2,27 +2,25 @@ from collections import deque
 
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        asc = deque()
-        desc = deque()
+        aq = deque()
+        dq = deque()
 
-        res = 0
-        left = 0
+        res = left = 0
         for right, num in enumerate(nums):
-            while asc and asc[-1] > num:
-                asc.pop()
-            asc.append(num)
+            while aq and aq[-1] > num:
+                aq.pop()
+            aq.append(num)
 
-            while desc and desc[-1] < num:
-                desc.pop()
-            desc.append(num)
+            while dq and dq[-1] < num:
+                dq.pop()
+            dq.append(num)
 
-            while desc[0] - asc[0] > limit:
-                if nums[left] == asc[0]:
-                    asc.popleft()
-                if nums[left] == desc[0]:
-                    desc.popleft()
+            while dq[0] - aq[0] > limit:
+                if nums[left] == dq[0]:
+                    dq.popleft()
+                if nums[left] == aq[0]:
+                    aq.popleft()
                 left += 1
-
             res = max(res, right - left + 1)
 
         return res
