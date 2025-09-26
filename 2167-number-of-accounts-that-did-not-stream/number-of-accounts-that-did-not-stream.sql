b280a93bfffdca1430f73d94ff40d941bbfa487a
Zax S <zacks.shen@gmail.com>
2025-09-26 07:19:18 -0700

    Time: 333 ms (35.48%) | Memory: 0B (100.00%) - LeetSync

--- 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -1,12 +1,11 @@
 select
-    count(account_id) as accounts_count
+    count(distinct account_id) as accounts_count
 from Subscriptions su
-where
-    su.end_date >= '2021-01-01'
-    and su.start_date < '2022-01-01'
+where start_date < '2022-01-01'
+    and end_date >= '2021-01-01'
     and not exists (
-    select 1
-    from Streams st
-    where su.account_id = st.account_id
-    and extract(year from st.stream_date) = 2021
-)
+        select 1
+        from Streams st
+        where su.account_id = st.account_id
+            and extract(year from stream_date) = 2021
+    )
