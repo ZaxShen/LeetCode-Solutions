6a7ee87819763b7cc4c684603b7f73f43845e5ad
Zax S <zacks.shen@gmail.com>
2025-09-25 20:03:42 -0700

    Time: 151 ms (83.74%) | Memory: 31.5 MB (49.30%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -3,23 +3,21 @@ from collections import deque
 class Solution:
     # O(n), O(k)
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
-        dq = deque()  # Stores indices (not values!)
         res = []
+        queue = deque()
         
-        for i in range(len(nums)):
-            # Remove indices outside current window
-            while dq and dq[0] < i - k + 1:
-                dq.popleft()
-            
-            # Remove indices with smaller values (maintain decreasing order)
-            while dq and nums[dq[-1]] < nums[i]:
-                dq.pop()
-            
-            # Add current index
-            dq.append(i)
-            
-            # Record maximum when window is complete
+        for i, num in enumerate(nums):
+            # Maintain monotonic decreasing
+            while queue and num > nums[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+
+            # Remove the oldest num exceeding the window size
+            if queue[0] + k == i:
+                queue.popleft()
+
+            # Only add to the answer once our window has reached size k
             if i >= k - 1:
-                res.append(nums[dq[0]])  # Front has max
-        
+                res.append(nums[queue[0]])
+
         return res
