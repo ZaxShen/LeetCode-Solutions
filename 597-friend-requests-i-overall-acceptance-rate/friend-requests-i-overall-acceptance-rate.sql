2445d94e56206020c85dd8854618e1c1bacbe506
Zax S <zacks.shen@gmail.com>
2025-09-26 08:09:23 -0700

    Time: 220 ms (41.22%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,8 +1,4 @@
 select
-    round(
-        coalesce(
-            (select count(distinct (requester_id, accepter_id)) from RequestAccepted)::NUMERIC
-            /
-            nullif((select count(distinct (sender_id, send_to_id)) from FriendRequest), 0),
-        0),
-    2) as accept_rate
+    ROUND(COALESCE((select count(distinct (requester_id, accepter_id)) from RequestAccepted)::NUMERIC
+    /
+    NULLIF((select count(distinct (sender_id, send_to_id)) from FriendRequest), 0), 0), 2) as accept_rate
