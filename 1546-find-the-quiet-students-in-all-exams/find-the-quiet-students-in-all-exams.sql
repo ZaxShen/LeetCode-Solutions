7abea41c42c8aabfb1e9b078927d79f287db79e4
Zax S <zacks.shen@gmail.com>
2025-09-29 14:38:32 -0700

    Time: 297 ms (9.71%) | Memory: 0B (100.00%) - LeetSync

--- 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
+++ 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
@@ -1,25 +1,21 @@
--- rank stduents
--- find students with at least one exma
--- output result and order by student_id
-
-with student_rank as (
+with cte as (
     select
-        e.exam_id,
-        e.student_id,
+        s.student_id,
         s.student_name,
+        e.exam_id,
         dense_rank() over(partition by e.exam_id order by e.score asc) as score_asc,
         dense_rank() over(partition by e.exam_id order by e.score desc) as score_desc
-    from Exam e
-    left join Student s using(student_id)
+    from Student s
+    join Exam e using(student_id)
 )
 select distinct
     student_id,
     student_name
-from student_rank
-where student_id not in (
+from cte s
+where not exists (
     select student_id
-    from student_rank 
-    where score_asc = 1
-        or score_desc = 1
+    from cte
+    where s.student_id = cte.student_id
+        and (score_asc = 1 or score_desc = 1)
 )
-order by student_id
+order by s.student_id asc
