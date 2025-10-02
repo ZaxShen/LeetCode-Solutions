3da7988cccadfa1be81041d6cff92c50b3c5a57c
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-02 17:29:36 -0400

    Time: 202 ms (29.67%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,7 +1,6 @@
-SELECT
+select
     patient_id,
     patient_name,
     conditions
 from Patients
-where conditions like 'DIAB1%' 
-    or conditions like '% DIAB1%'
+where conditions ~ '(^|[*[:space:]])DIAB1'
