cd5bc7a779b8d422002364313df77aa71fc6c6a3
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-03 11:19:48 -0400

    Time: 1080 ms (26.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -0,0 +1,11 @@
+with cte as (
+    select
+        seller_id,
+        dense_rank() over(order by sum(price) desc) as total_price_desc
+    from Sales
+    group by seller_id
+)
+select
+    seller_id
+from cte
+where total_price_desc = 1
