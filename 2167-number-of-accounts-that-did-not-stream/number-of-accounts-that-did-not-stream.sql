a919e4324b3de2a6eac9287d52e3bd14b468d672
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-03 16:43:22 -0400

    Time: 318 ms (51.22%) | Memory: 0B (100.00%) - LeetSync

--- 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -1,13 +1,11 @@
--- get subscription valid in 2021
--- get streams in 2021
 select
     count(*) as accounts_count
 from Subscriptions su
-where su.start_date < '2022-01-01'
-    and su.end_date >= '2021-01-01'
+where start_date < '2022-01-01'
+    and end_date >= '2021-01-01'
     and not exists (
-        select 1
+        select account_id
         from Streams st
-        where su.account_id = st.account_id
-            and extract(year from st.stream_date) = 2021
+        where extract(year from stream_date) = 2021
+            and su.account_id = st.account_id
     )
