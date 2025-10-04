46d9fe36b5a55ba5cadb64c81510f4af7cb817c2
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-04 17:55:05 -0400

    Time: 859 ms (92.00%) | Memory: 0B (100.00%) - LeetSync

--- 1173-sales-analysis-ii/sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -1,7 +1,7 @@
 select
     s.buyer_id
 from Sales s
-    join Product p on s.product_id = p.product_id
+join Product p on s.product_id = p.product_id
 group by s.buyer_id
-having COUNT(*) filter (where product_name = 'iPhone') = 0
-    and COUNT(*) filter (where product_name = 'S8') > 0
+    having count(*) filter(where p.product_name = 'S8') > 0
+    and count(*) filter(where p.product_name = 'iPhone') = 0
