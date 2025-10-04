f3ba3df615b76a2d214a12d8e1d5dc6f8b7af18f
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-04 12:21:14 -0400

    Time: 348 ms (55.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2452-product-sales-analysis-v/product-sales-analysis-v.sql
@@ -0,0 +1,7 @@
+select
+    s.user_id,
+    sum(s.quantity * p.price) as spending
+from Sales s
+    join Product p on s.product_id = p.product_id
+group by s.user_id
+order by spending desc, user_id asc
