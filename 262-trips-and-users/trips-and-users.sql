3cf742602ff0c357304205b9a2423e2b81d0b425
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-03 20:21:44 -0400

    Time: 176 ms (99.31%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,11 +1,11 @@
 select
     t.request_at as Day,
-    round(count(*) filter(where t.status like 'cancelled%')::numeric / count(*), 2) as "Cancellation Rate"
+    round(count(*) FILTER (where t.status ~ '^cancelled')::NUMERIC / count(*), 2) as "Cancellation Rate"
 from Trips t
-    join Users c on t.client_id = c.users_id 
+    join Users c on t.client_id = c.users_id
         and c.banned = 'No'
         and c.role = 'client'
-    join Users d on t.driver_id = d.users_id 
+    join Users d on t.driver_id = d.users_id
         and d.banned = 'No'
         and d.role = 'driver'
 where t.request_at between '2013-10-01' and '2013-10-03'
