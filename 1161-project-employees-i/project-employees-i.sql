2c59ac007797620afff8cd60491fe4a3a5325095
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-03 21:34:23 -0400

    Time: 418 ms (99.97%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,4 +1,6 @@
-select project_id, round(avg(experience_years), 2) as average_years
-from project
-join employee using (employee_id)
-group by project_id
+select
+    p.project_id,
+    coalesce(round(avg(e.experience_years), 2), 0) as average_years
+from Project p
+    left join Employee e using (employee_id)
+group by p.project_id
