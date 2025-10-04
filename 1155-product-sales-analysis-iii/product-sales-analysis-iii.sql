63defdf0c7ec4592912986ad6ed5de760b4e229d
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-04 11:51:17 -0400

    Time: 837 ms (90.47%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,15 +1,14 @@
-with cte as (
-    select
-        product_id,
-        min(year) as year
-    from Sales
-    group by product_id
-)
-select
-    cte.product_id,
-    cte.year as first_year,
-    s.quantity,
-    s.price
-from cte
-    join Sales s on cte.product_id = s.product_id
-        and cte.year = s.year
+SELECT
+    product_id,
+    YEAR AS first_year,
+    quantity,
+    price
+FROM Sales
+WHERE
+    (product_id, YEAR) IN (
+        SELECT
+            product_id,
+            MIN(YEAR) AS first_year
+        FROM Sales
+        GROUP BY product_id
+    )
