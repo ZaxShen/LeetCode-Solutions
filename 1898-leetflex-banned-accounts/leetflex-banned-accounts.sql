9e04c13e7ba4593c8fe72937c06475b4f2374cc4
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-04 11:06:21 -0400

    Time: 345 ms (32.14%) | Memory: 0B (100.00%) - LeetSync

--- 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -2,5 +2,7 @@ select distinct l1.account_id
 from LogInfo l1
     join LogInfo l2 on l1.account_id = l2.account_id
         and l1.ip_address != l2.ip_address
-where l1.logout >= l2.login
+where
+    l1.logout >= l2.login
+    -- and l1.login >= l2.logout
     and l2.logout >= l1.login
