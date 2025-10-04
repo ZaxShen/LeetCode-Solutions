8a81d64afa86392afc1803155b98113f46eedfce
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-03 20:50:06 -0400

    Time: 553 ms (90.83%) | Memory: 0B (100.00%) - LeetSync

--- 1198-unpopular-books/unpopular-books.sql
+++ 1198-unpopular-books/unpopular-books.sql
@@ -4,8 +4,9 @@ SELECT
 FROM Books b
 LEFT JOIN Orders o 
     ON b.book_id = o.book_id 
-    AND o.dispatch_date BETWEEN '2018-06-23'::DATE AND '2019-06-23'::DATE
+    AND o.dispatch_date <= '2019-06-23'
+    AND o.dispatch_date >= '2019-06-23'::DATE - INTERVAL '1 year'
 WHERE b.available_from < '2019-06-23'::DATE - INTERVAL '1 month'
 GROUP BY b.book_id, b.name
 HAVING COALESCE(SUM(o.quantity), 0) < 10
-ORDER BY b.book_id;
+ORDER BY b.book_id
