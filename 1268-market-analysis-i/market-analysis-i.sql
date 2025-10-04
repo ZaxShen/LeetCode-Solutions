98090d99f8b801fae5c4d5e38e40b29d62547c82
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-04 16:30:34 -0400

    Time: 517 ms (83.35%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -4,6 +4,6 @@ select
     count(o.order_id) as orders_in_2019
 from Users u
     left join Orders o on u.user_id = o.buyer_id
-    and extract(year from o.order_date) = 2019
+        and extract(year from o.order_date) = 2019
 group by u.user_id, u.join_date
 order by u.user_id
