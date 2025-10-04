f19d7924b184f881536807b33fe87d8fce008900
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-04 09:59:38 -0400

    Time: 569 ms (39.24%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,7 +1,8 @@
-SELECT 
-    LEAST(from_id, to_id) AS person1,
-    GREATEST(from_id, to_id) AS person2,
-    COUNT(*) AS call_count,
-    SUM(duration) AS total_duration
-FROM Calls
-GROUP BY person1, person2;
+select
+    least(from_id, to_id) as person1,
+    greatest(from_id, to_id) as person2,
+    count(*) as call_count,
+    sum(duration) as total_duration
+from Calls
+group by person1, person2
+order by person1, person2
