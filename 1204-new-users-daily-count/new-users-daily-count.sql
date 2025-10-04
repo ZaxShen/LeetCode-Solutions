3d790574984fef357022e5595d0af80d36352a0a
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-03 20:29:19 -0400

    Time: 462 ms (77.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1204-new-users-daily-count/new-users-daily-count.sql
@@ -0,0 +1,15 @@
+with cte as (
+select
+    user_id,
+    min(activity_date) as first_login
+from Traffic
+where activity = 'login'
+group by user_id
+)
+select
+    first_login as login_date,
+    count(*) as user_count
+from cte
+where '2019-06-30' - first_login <= 90
+group by login_date
+order by login_date
