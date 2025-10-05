70c7311fb244fd56f5b0dbe29016cb349b9f257d
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-05 15:11:02 -0400

    Time: 309 ms (62.21%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,12 +1,7 @@
 select
-    u.name,
-    coalesce(sum(distance), 0) as travelled_distance
-from
-    Users u
+    u."name",
+    coalesce(sum(r.distance), 0) as travelled_distance
+from Users u
     left join Rides r on u.id = r.user_id
-group by
-    u.id,
-    u.name
-order by
-    travelled_distance desc,
-    u.name asc
+group by u.id, u."name"
+order by travelled_distance desc, "name" asc
