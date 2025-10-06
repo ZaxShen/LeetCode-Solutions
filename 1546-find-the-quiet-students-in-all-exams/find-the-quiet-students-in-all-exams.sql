b2e034a130fe8bae427834ea847f3700dea15da3
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 17:25:52 -0400

    Time: 207 ms (75.96%) | Memory: 0B (100.00%) - LeetSync

--- 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
+++ 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
@@ -2,20 +2,17 @@ with cte as (
     select
         s.student_id,
         s.student_name,
-        e.exam_id,
-        dense_rank() over(partition by e.exam_id order by e.score asc) as score_asc,
-        dense_rank() over(partition by e.exam_id order by e.score desc) as score_desc
-    from Student s
-    join Exam e using(student_id)
+        dense_rank() over(partition by e.exam_id order by e.score asc) as socre_asc,
+        dense_rank() over(partition by e.exam_id order by e.score desc) as socre_desc
+    from
+        Student s
+        join Exam e on s.student_id = e.student_id
 )
 select distinct
     student_id,
     student_name
-from cte s
-where not exists (
-    select student_id
-    from cte
-    where s.student_id = cte.student_id
-        and (score_asc = 1 or score_desc = 1)
+from cte
+where student_id not in (
+    select student_id from cte where socre_asc = 1 or socre_desc = 1
 )
-order by s.student_id asc
+order by student_id
