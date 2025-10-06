5949dd8f8a14842b9388131182bc16a586c52b86
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 10:58:32 -0400

    Time: 267 ms (27.68%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -1,20 +1,27 @@
-with cte as (
-    select
-        o.customer_id,
-        c.name,
-        to_char(o.order_date, 'YYYY-MM') as order_month,
-        sum(o.quantity * p.price) as total
-    from
-        Orders o
-        left join Customers c on o.customer_id = c.customer_id
-        left join Product p on o.product_id = p.product_id
-    where o.order_date between '2020-06-01'::DATE and '2020-07-31'::DATE
-    group by o.customer_id, c.name, to_char(o.order_date, 'YYYY-MM')
-)
-select
+WITH
+    cte AS (
+        SELECT
+            o.customer_id,
+            c.name,
+            TO_CHAR(o.order_date, 'YYYY-MM') AS order_month,
+            SUM(o.quantity * p.price) AS total
+        FROM
+            Orders o
+            JOIN customers c USING (customer_id)
+            JOIN product p USING (product_id)
+        WHERE o.order_date BETWEEN '2020-06-01'::DATE AND '2020-07-31'::DATE
+        GROUP BY
+            o.customer_id,
+            c.name,
+            TO_CHAR(o.order_date, 'YYYY-MM')
+    )
+SELECT
     customer_id,
     name
-from cte
-group by customer_id, name
-    having count(*) = 2
-    and min(total) >= 100
+FROM cte
+GROUP BY
+    customer_id,
+    name
+HAVING
+    COUNT(*) = 2
+    AND MIN(total) >= 100
