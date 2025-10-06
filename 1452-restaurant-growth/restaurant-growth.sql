fd39c1bb9f44efb90a080d568cbb73ff5677fb7c
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 07:59:06 -0400

    Time: 178 ms (89.43%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,14 +1,16 @@
 with cte as (
     select
         visited_on,
-        sum(sum(amount)) over(order by visited_on rows between 6 preceding and current row) as rolling_sum_7day,
-        row_number() over(order by visited_on) as day_num
+        sum(amount) as daily_total,
+        sum(sum(amount)) over(order by visited_on rows between 6 preceding and current row) as rolling_sum_7d,
+        avg(sum(amount)) over(order by visited_on rows between 6 preceding and current row) as rolling_avg_7d,
+        row_number() over(order by visited_on) as num_day
     from Customer
     group by visited_on
 )
 select
     visited_on,
-    rolling_sum_7day as amount,
-    round(rolling_sum_7day / 7, 2) as average_amount
+    rolling_sum_7d as amount,
+    round(rolling_avg_7d, 2) as average_amount
 from cte
-where day_num >= 7
+where num_day >= 7
