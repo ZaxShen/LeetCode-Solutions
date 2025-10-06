ef956b656ac97d4bac8e2cfe0d45d31bfff393cd
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 09:08:14 -0400

    Time: 228 ms (65.89%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,14 +1,13 @@
 select
-    c.customer_id,
+    o.customer_id,
     c.customer_name
 from
-    Customers c
-    join Orders o on c.customer_id = o.customer_id
-group by
-    c.customer_id,
+    Orders o
+    left join Customers c on o.customer_id = c.customer_id
+group by 
+    o.customer_id, 
     c.customer_name
-having
-    sum((o.product_name = 'A')::INT) > 0
+having sum((o.product_name = 'A')::INT) > 0
     and sum((o.product_name = 'B')::INT) > 0
     and sum((o.product_name = 'C')::INT) = 0
-order by customer_id
+order by o.customer_id
