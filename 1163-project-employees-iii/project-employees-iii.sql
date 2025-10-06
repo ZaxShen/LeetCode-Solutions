ec36595cbf4dcd255655452b4c3cf6abd38eb9ff
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 16:59:22 -0400

    Time: 292 ms (76.24%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,14 +1,17 @@
--- rank exp
--- select
-with cte as (
-    select
+WITH cte AS (
+    SELECT
         project_id,
         p.employee_id,
-        dense_rank() over(partition by p.project_id order by e.experience_years desc) as exp_desc
-    from Project p
-        join Employee e using(employee_id)
+        DENSE_RANK() OVER (PARTITION BY p.project_id ORDER BY e.experience_years DESC) AS exp_desc
+    FROM
+        Project p
+        JOIN Employee e USING (employee_id)
 )
-select project_id, employee_id
-from cte
-where exp_desc = 1
-order by project_id asc, employee_id asc
+SELECT
+    project_id,
+    employee_id
+FROM cte
+WHERE exp_desc = 1
+ORDER BY
+    project_id ASC,
+    employee_id ASC
