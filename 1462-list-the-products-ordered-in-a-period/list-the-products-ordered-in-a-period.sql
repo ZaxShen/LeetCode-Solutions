a4efb9b4192cf741926799e9a695c47ccd6904cb
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 08:38:10 -0400

    Time: 368 ms (5.17%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,9 +1,9 @@
 select
     p.product_name,
-    sum(unit) as unit
+    sum(o.unit) as unit
 from
     Orders o
-    join Products p on o.product_id = p.product_id
+    left join Products p on o.product_id = p.product_id
 where to_char(o.order_date, 'YYYY-MM') = '2020-02'
-group by p.product_name
-    having sum(unit) >= 100
+group by o.product_id, p.product_name
+having sum(o.unit) >= 100
