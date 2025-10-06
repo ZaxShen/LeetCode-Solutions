d18a22ebe3e6ae01fbf9432796a20cc6e35a3ea6
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 00:48:19 -0400

    Time: 256 ms (74.87%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,27 +1,19 @@
-SELECT
-    ROUND(SUM(tiv_2016)::NUMERIC, 2) AS tiv_2016
-FROM
-    Insurance
+SELECT ROUND(SUM(tiv_2016)::NUMERIC, 2) AS tiv_2016
+FROM Insurance
 WHERE
     tiv_2015 IN (
-        SELECT
-            tiv_2015
-        FROM
-            Insurance
-        GROUP BY
-            1
-        HAVING
-            COUNT(*) > 1
+        SELECT tiv_2015
+        FROM Insurance
+        GROUP BY 1
+        HAVING COUNT(*) > 1
     )
     AND (lat, lon) IN (
         SELECT
             lat,
             lon
-        FROM
-            Insurance
+        FROM Insurance
         GROUP BY
             lat,
             lon
-        HAVING
-            COUNT(*) = 1
+        HAVING COUNT(*) = 1
     );
