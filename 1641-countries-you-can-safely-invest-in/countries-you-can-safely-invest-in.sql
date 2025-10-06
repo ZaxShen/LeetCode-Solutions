fcd1a59e974bc16642981756e3afb41724d84123
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 11:11:21 -0400

    Time: 489 ms (39.13%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,6 +1,7 @@
 select co.name as country
-from Calls ca
+from
+    Calls ca
     join Person p on p.id in (ca.caller_id, ca.callee_id)
-    join Country co on co.country_code = SUBSTRING(p.phone_number, 1, 3)
-group by co.name
-    having avg(ca.duration) > (select avg(duration) from Calls)
+    join Country co on substring(p.phone_number, 1, 3) = co.country_code
+group by country
+having avg(duration) > (select avg(duration) from Calls)
