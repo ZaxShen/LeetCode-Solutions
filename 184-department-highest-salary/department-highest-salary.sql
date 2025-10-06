e0a45ac769ff2dfc61bbbfac3bd23f189814b6e9
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 08:34:56 -0400

    Time: 230 ms (77.77%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,16 +1,16 @@
-with cte as (
-    select
-        d.name as Department,
-        e.name as Employee,
-        e.salary as Salary,
-        dense_rank() over(partition by d.id order by e.salary desc) as salary_desc
-    from
+WITH cte AS (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS salary_desc
+    FROM
         Employee e
-        left join Department d on e.departmentId = d.id
+        LEFT JOIN Department d ON e.departmentId = d.id
 )
-select
+SELECT
     Department,
     Employee,
     Salary
-from cte
-where salary_desc = 1
+FROM cte
+WHERE salary_desc = 1
