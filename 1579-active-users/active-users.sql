e987901245b0d0c1cc67cbfe98b95028c97aebbd
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 00:07:44 -0400

    Time: 464 ms (97.22%) | Memory: 0B (100.00%) - LeetSync

--- 1579-active-users/active-users.sql
+++ 1579-active-users/active-users.sql
@@ -1,15 +1,21 @@
-with cte as (
-    select
-        id,
-        login_date - interval '1 day' * row_number() over(partition by id order by login_date) as grp
-    from (select distinct id, login_date from Logins)
-)
-select distinct
+WITH
+    cte AS (
+        SELECT
+            id,
+            login_date - INTERVAL '1 day' * ROW_NUMBER() OVER (
+                PARTITION BY id ORDER BY login_date
+            ) AS grp
+        FROM (SELECT DISTINCT id, login_date FROM Logins)
+    )
+SELECT DISTINCT
     cte.id,
     a.name
-from
+FROM
     cte
-    join Accounts a on cte.id = a.id
-group by cte.id, a.name, grp
-    having count(grp) >= 5
-order by cte.id
+    JOIN Accounts a ON cte.id = a.id
+GROUP BY
+    cte.id,
+    a.name,
+    grp
+HAVING COUNT(grp) >= 5
+ORDER BY cte.id
