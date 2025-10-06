faedf04ba41a8eed809ddf1705fcd0eacc38e05a
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 16:08:17 -0400

    Time: 678 ms (39.05%) | Memory: 0B (100.00%) - LeetSync

--- 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
+++ 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
@@ -1,22 +1,22 @@
-with cte as (
-select
-    p.product_name,
-    o.product_id,
-    o.order_id,
-    o.order_date,
-    dense_rank() over(partition by o.product_id order by o.order_date desc) as order_date_desc
-from
-    Orders o
-    join Products p on o.product_id = p.product_id
+WITH cte AS (
+    SELECT
+        p.product_name,
+        o.product_id,
+        o.order_id,
+        o.order_date,
+        DENSE_RANK() OVER (PARTITION BY o.product_id ORDER BY o.order_date DESC) AS order_date_desc
+    FROM
+        Orders o
+        JOIN Products p ON o.product_id = p.product_id
 )
-select
+SELECT
     product_name,
     product_id,
     order_id,
     order_date
-from cte
-where order_date_desc = 1
-order by
+FROM cte
+WHERE order_date_desc = 1
+ORDER BY
     product_name,
     product_id,
     order_id
