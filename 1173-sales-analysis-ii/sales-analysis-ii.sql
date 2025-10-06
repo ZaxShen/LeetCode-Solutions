1984dfe7ea05878721752d8d6f0149d22484cf0b
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 08:40:23 -0400

    Time: 939 ms (62.63%) | Memory: 0B (100.00%) - LeetSync

--- 1173-sales-analysis-ii/sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -1,7 +1,7 @@
-select
-    s.buyer_id
-from Sales s
-join Product p on s.product_id = p.product_id
-group by s.buyer_id
-    having sum((p.product_name = 'S8')::INT) > 0
+select buyer_id
+from
+    Sales s
+    join Product p on s.product_id = p.product_id
+group by buyer_id
+having sum((p.product_name = 'S8')::INT) != 0
     and sum((p.product_name = 'iPhone')::INT) = 0
