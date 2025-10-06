2ccf653ce112ce09962d07f9cae079fe88c91de2
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 13:56:07 -0400

    Time: 265 ms (16.26%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,8 +1,8 @@
 select
     id,
     case
-        when p_id is NULL then 'Root'
-        when id in (select p_id from Tree) then 'Inner'
+        when p_id is null then 'Root'
+        when id in (select distinct p_id from Tree) then 'Inner'
         else 'Leaf'
-    end as type
+    end as "type"
 from Tree
