c52c58acd379a9dd73b02dce8bce18fd03685065
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 01:41:37 -0400

    Time: 226 ms (38.36%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -2,7 +2,4 @@ select coalesce(e.employee_id, s.employee_id) as employee_id
 from
     Employees e
     full outer join Salaries s on e.employee_id = s.employee_id
-where
-    e."name" is null
-    or s.salary is null
-order by employee_id
+where e.name is NULL or s.salary is null
