9945878db9162f72b83b3669646efda2c6edcc0b
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-05 21:00:22 -0400

    Time: 216 ms (96.05%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,13 +1,5 @@
-with cte as (
-    select
-        employee_id,
-        department_id
-    from Employee
-    where primary_flag = 'Y'
-)
-select * from cte
-union
-select employee_id, department_id
+select distinct on (employee_id)
+    employee_id,
+    department_id
 from Employee
-where employee_id not in (select employee_id from cte)
-order by employee_id
+order by employee_id, primary_flag desc
