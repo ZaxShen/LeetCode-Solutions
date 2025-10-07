c273aa8e17a96ecca7e4b5feaafd016e3a8cb537
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-07 18:59:34 -0400

    Time: 164 ms (67.74%) | Memory: 0B (100.00%) - LeetSync

--- 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,13 +1,10 @@
-WITH RECURSIVE
-    cte AS (
-        SELECT employee_id FROM Employees WHERE manager_id = 1
-        UNION
-        SELECT e.employee_id
-        FROM
-            Employees e
-            JOIN cte c ON e.manager_id = c.employee_id
-    )
-SELECT employee_id
-FROM cte
-WHERE employee_id != 1
-ORDER BY employee_id
+with recursive cte as (
+    select 1 as employee_id
+    union
+    select e.employee_id
+    from Employees e
+    join cte on e.manager_id = cte.employee_id
+)
+select employee_id
+from cte
+where employee_id != 1
