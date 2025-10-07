4d2289986280d5b4b40dbf0a4b90f36d6a82ccad
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-07 19:29:50 -0400

    Time: 212 ms (40.00%) | Memory: 0B (100.00%) - LeetSync

--- 1759-find-the-missing-ids/find-the-missing-ids.sql
+++ 1759-find-the-missing-ids/find-the-missing-ids.sql
@@ -1,6 +1,13 @@
-SELECT s.ids
-FROM 
-    GENERATE_SERIES(1, (SELECT MAX(customer_id) FROM Customers)) AS s(ids)
-    LEFT JOIN Customers c on s.ids = c.customer_id
+WITH RECURSIVE cte AS (
+    SELECT 1 AS ids
+    UNION
+    SELECT ids + 1
+    FROM cte
+    WHERE ids < (SELECT MAX(customer_id) FROM Customers)
+)
+SELECT cte.ids
+FROM
+    cte
+    LEFT JOIN Customers c on cte.ids = c.customer_id
 WHERE c.customer_id IS NULL
 ORDER BY ids
