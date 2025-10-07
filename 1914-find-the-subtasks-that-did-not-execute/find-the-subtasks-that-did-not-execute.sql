7e5e1d7be4c856f1a7023336f7542af1e6bc4f45
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-07 19:13:51 -0400

    Time: 366 ms (35.71%) | Memory: 0B (100.00%) - LeetSync

--- 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
+++ 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
@@ -3,8 +3,7 @@ select
     s.subtask_id
 from
     Tasks t
-    cross join generate_series(1, t.subtasks_count) as s(subtask_id)
+    cross join generate_series(1, subtasks_count) as s(subtask_id)
     left join Executed e on t.task_id = e.task_id
         and s.subtask_id = e.subtask_id
 where e.subtask_id is NULL
-order by task_id
