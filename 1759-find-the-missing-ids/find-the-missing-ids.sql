23e5b44e167356c0931dc94af5d8149c4be7908b
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-07 19:27:37 -0400

    Time: 203 ms (57.50%) | Memory: 0B (100.00%) - LeetSync

--- 1759-find-the-missing-ids/find-the-missing-ids.sql
+++ 1759-find-the-missing-ids/find-the-missing-ids.sql
@@ -1,7 +1,6 @@
-select distinct s.ids
-from
-    Customers c1
-    cross join generate_series(1, (select max(customer_id) from Customers)) as s(ids)
-    left join Customers c2 on s.ids = c2.customer_id
-where c2.customer_id IS NULL
-order by ids
+SELECT s.ids
+FROM 
+    GENERATE_SERIES(1, (SELECT MAX(customer_id) FROM Customers)) AS s(ids)
+    LEFT JOIN Customers c on s.ids = c.customer_id
+WHERE c.customer_id IS NULL
+ORDER BY ids
