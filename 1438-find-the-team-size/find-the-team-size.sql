04463e86e74090e6440607f418e87512f1394d0e
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 20:29:41 -0400

    Time: 164 ms (88.70%) | Memory: 0B (100.00%) - LeetSync

--- 1438-find-the-team-size/find-the-team-size.sql
+++ 1438-find-the-team-size/find-the-team-size.sql
@@ -1,4 +1,4 @@
-SELECT
+select
     employee_id,
-    COUNT(employee_id) OVER (PARTITION BY team_id) AS team_size
-FROM Employee
+    count(employee_id) over(partition by team_id) as team_size
+from Employee
