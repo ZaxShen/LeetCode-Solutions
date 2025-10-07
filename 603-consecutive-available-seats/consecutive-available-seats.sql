81762c9d38d14d4305b88fe45261ebc0d2ece8af
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-06 20:48:22 -0400

    Time: 178 ms (92.49%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,17 +1,16 @@
 with grps as (
     select
-        *,
-        seat_id - row_number() over(partition by free order by seat_id) as grp
+        seat_id,
+        seat_id - row_number() over(order by seat_id) as grp
     from Cinema
     where free = 1
 ),
-seat_counts as (
+cte as (
     select
         seat_id,
-        count(seat_id) over(partition by grp) as group_size
+        count(*) over(partition by grp) as grp_size
     from grps
 )
 select seat_id
-from seat_counts
-where group_size >= 2
-order by seat_id
+from cte
+where grp_size >= 2
