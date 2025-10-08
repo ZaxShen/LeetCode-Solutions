3c3b4cc3cc61253353c3e974117be86a6c565c77
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-08 08:41:34 -0400

    Time: 43 ms (72.14%) | Memory: 21.3 MB (50.71%) - LeetSync

--- 1061-number-of-valid-subarrays/number-of-valid-subarrays.py
+++ 1061-number-of-valid-subarrays/number-of-valid-subarrays.py
@@ -1,13 +1,12 @@
 class Solution:
     def validSubarrays(self, nums: List[int]) -> int:
-        asc = []
-        res = 0
+        stack = []
+        count = 0
 
-        for i, v in enumerate(nums):
-            while asc and nums[asc[-1]] > v:
-                asc.pop()
-            asc.append(i)
-            res += len(asc)
+        for num in nums:
+            while stack and stack[-1] > num:
+                stack.pop()
+            stack.append(num)
+            count += len(stack)
 
-        return res
-
+        return count
