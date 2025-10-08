61021182dd22e55b9c2f8806e421cbbeea3a3156
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-08 10:18:34 -0400

    Time: 224 ms (24.85%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -1,20 +1,14 @@
-WITH all_friends AS (
-    -- Get all friends of user 1 (bidirectional)
-    SELECT user2_id AS friend_id
-    FROM Friendship
-    WHERE user1_id = 1
-    
-    UNION
-    
-    SELECT user1_id AS friend_id
-    FROM Friendship
-    WHERE user2_id = 1
+with cte as (
+    select
+        least(user1_id, user2_id) as user1_id,
+        greatest(user1_id, user2_id) as user2_id
+    from Friendship
 )
-SELECT DISTINCT l.page_id AS recommended_page
-FROM all_friends f
-JOIN Likes l ON f.friend_id = l.user_id
-WHERE l.page_id NOT IN (
-    SELECT page_id
-    FROM Likes
-    WHERE user_id = 1
-)
+select distinct l.page_id as recommended_page
+from
+    cte c
+    join Likes l on c.user2_id = l.user_id
+where c.user1_id = 1
+    and l.page_id not in (
+        select page_id from Likes where user_id = 1
+    )
