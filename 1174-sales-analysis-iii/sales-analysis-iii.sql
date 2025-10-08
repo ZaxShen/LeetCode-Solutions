2155036c7199f0dde4d15f47e6715103734cfab0
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-08 17:40:59 -0400

    Time: 880 ms (82.98%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,9 +1,16 @@
+-- product_id is PK of Product, and FK of Sales
+-- product_id, product_name, sale_date; quantity may be zero all null?
+-- filter Sales finally join Product to opt
+
+with cte as (
 select
-    s.product_id,
-    p.product_name
-from
-    Sales s
-    left join Product p on s.product_id = p.product_id
-group by s.product_id, p.product_name
-having min(s.sale_date) >= '2019-01-01'::DATE
-    and max(s.sale_date) <= '2019-03-31'::DATE
+    s.product_id
+from Sales s
+group by s.product_id
+having
+    min(s.sale_date) >= '2019-01-01'::DATE
+    and max(s.sale_date) < '2019-04-01'::DATE
+)
+select cte.product_id, p.product_name
+from cte
+join Product p on cte.product_id = p.product_id
