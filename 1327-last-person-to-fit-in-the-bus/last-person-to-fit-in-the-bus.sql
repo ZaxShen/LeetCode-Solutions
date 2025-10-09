6761ffa83f56e6771ba9f9ce6e5c966d336acf01
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-09 01:38:56 -0400

    Time: 497 ms (39.22%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -2,11 +2,11 @@ with cte as (
     select
         person_name,
         turn,
-        sum(weight) over(order by turn asc) as total_weight
+        sum(weight) over(order by turn) as acc_weight
     from Queue
 )
 select person_name
 from cte
-where total_weight <= 1000
+where acc_weight <= 1000
 order by turn desc
 limit 1
