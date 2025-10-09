56b8eb342fab78e399fea117db61eaf3884d38f3
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-09 01:05:46 -0400

    Time: 670 ms (91.95%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -6,7 +6,8 @@ select
         when e.operator = '<' and v1.value < v2.value then 'true'
         else 'false'
     end as value
-from
+from 
     Expressions e
     join Variables v1 on e.left_operand = v1.name
     join Variables v2 on e.right_operand = v2.name
+
