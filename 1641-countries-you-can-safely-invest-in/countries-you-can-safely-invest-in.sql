9cc235e59280fcf3762d3f7cba84d3e35d63dc55
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-09 01:17:07 -0400

    Time: 493 ms (36.23%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,13 +1,23 @@
-with cte(call_id, duration) as (
-    select caller_id, duration from Calls
-    union all
-    select callee_id, duration from Calls
-)
-select
-    co.name as country
-from
-    cte c
-    join Person p on c.call_id = p.id
-    join Country co on substring(p.phone_number, 1, 3) = co.country_code
-group by co.name
-    having avg(duration) > (select avg(duration) from Calls)
+-- with cte(call_id, duration) as (
+--     select caller_id, duration from Calls
+--     union all
+--     select callee_id, duration from Calls
+-- )
+-- select
+--     co.name as country
+-- from
+--     cte c
+--     join Person p on c.call_id = p.id
+--     join Country co on substring(p.phone_number, 1, 3) = co.country_code
+-- group by co.name
+--     having avg(duration) > (select avg(duration) from Calls)
+
+SELECT
+    co.name AS country
+    -- *
+FROM
+    Calls ca
+    JOIN Person p ON p.id IN (ca.caller_id, ca.callee_id)
+    JOIN Country co ON SUBSTRING(p.phone_number, 1, 3) = co.country_code
+GROUP BY co.name
+HAVING AVG(duration) > (SELECT AVG(duration) FROM Calls)
