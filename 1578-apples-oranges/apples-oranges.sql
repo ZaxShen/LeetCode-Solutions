7251e37d2006948db53b048839ee97310cd30a86
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-09 01:57:22 -0400

    Time: 199 ms (28.22%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,7 +1,11 @@
+with cte(sale_date, amount) as (
+    select sale_date, sold_num from Sales where fruit = 'apples'
+    union all
+    select sale_date, -sold_num from Sales where fruit = 'oranges'
+)
 select
     sale_date,
-    sum(case when fruit = 'apples' then sold_num else 0 end) - 
-    sum(case when fruit = 'oranges' then sold_num else 0 end) as diff
-from Sales
+    sum(amount) as diff
+from cte
 group by sale_date
 order by sale_date
