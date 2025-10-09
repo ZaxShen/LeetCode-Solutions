949aa8e4964b6948d9357734f8b52058e4f36957
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-09 14:11:52 -0400

    Time: 220 ms (84.68%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 select
     query_name,
-    round(avg(rating::numeric / position), 2) as quality,
-    round(avg((rating < 3)::int) * 100, 2) as poor_query_percentage
+    round(avg(rating::NUMERIC / position), 2) as quality,
+    round(avg((rating < 3)::INT) * 100, 2) as poor_query_percentage
 from Queries
 group by query_name
