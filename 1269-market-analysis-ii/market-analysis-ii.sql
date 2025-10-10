d01051f40ecf7847884a574866895287a5c66ac8
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-10 17:08:59 -0400

    Time: 489 ms (95.00%) | Memory: 0B (100.00%) - LeetSync

--- 1269-market-analysis-ii/market-analysis-ii.sql
+++ 1269-market-analysis-ii/market-analysis-ii.sql
@@ -1,19 +1,19 @@
-with sec_item_sold as (
+with cte as (
     select
         seller_id,
-        order_date,
         item_id,
-        dense_rank() over(partition by seller_id order by order_date) as order_date_asc
+        dense_rank() over(partition by seller_id order by order_date) as sell_date_rnk
     from Orders
 )
 select
     u.user_id as seller_id,
     case
-        when u.favorite_brand = i.item_brand then 'yes'
+        when i.item_brand = u.favorite_brand then 'yes'
         else 'no'
     end as "2nd_item_fav_brand"
-from Users u
-    left join sec_item_sold s on u.user_id = s.seller_id
-        and s.order_date_asc = 2
-    left join Items i on s.item_id = i.item_id
-order by u.user_id
+from
+    Users u
+    left join cte c on u.user_id = c.seller_id
+        and c.sell_date_rnk = 2
+    left join Items i on c.item_id = i.item_id
+order by seller_id
