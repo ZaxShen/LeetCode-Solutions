4bad81df2ba01c2e62d89a8db05a420fa9211d28
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-10 21:30:41 -0400

    Time: 3 ms (44.95%) | Memory: 17.6 MB (96.55%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,6 +1,7 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         stack = []
+
         for char in s:
             if stack and stack[-1] != char and stack[-1].lower() == char.lower():
                 stack.pop()
