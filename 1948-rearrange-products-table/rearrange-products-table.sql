bb461683c5a54b978cde9f9bab9059e957c5e08c
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-11 11:32:24 -0400

    Time: 214 ms (77.45%) | Memory: 0B (100.00%) - LeetSync

--- 1948-rearrange-products-table/rearrange-products-table.sql
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -4,18 +4,14 @@ select
     store1 as price
 from Products
 where store1 is not null
-
 union
-
 select
     product_id,
     'store2' as store,
     store2 as price
 from Products
 where store2 is not null
-
-union
-
+union 
 select
     product_id,
     'store3' as store,
