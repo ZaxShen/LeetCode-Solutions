3b1dc2f25aae940bce51b8be29b85b0a0ac4436e
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-11 23:27:02 -0400

    Time: 83 ms (80.48%) | Memory: 18.4 MB (8.02%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,8 +5,7 @@ class Solution:
         if len(word1) != len(word2):
             return False
 
-        c1 = Counter(word1)
-        c2 = Counter(word2)
+        c1, c2 = Counter(word1), Counter(word2)
 
         if c1.keys() != c2.keys():
             return False
