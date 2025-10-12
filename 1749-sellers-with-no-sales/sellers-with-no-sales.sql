b94d95a7d0e7f3c31f293469dd664b389860bf24
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-12 14:19:36 -0400

    Time: 774 ms (94.67%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,9 +1,10 @@
-select seller_name
-from Seller
-where not exists (
-    select 1
-    from Orders
-    where extract(year from sale_date) = 2020
-    and Seller.seller_id = Orders.seller_id
-)
-order by seller_name
+SELECT s.seller_name
+FROM
+    Seller s
+WHERE
+    s.seller_id NOT IN (
+        SELECT seller_id
+        FROM Orders
+        WHERE EXTRACT(YEAR FROM sale_date) = 2020
+    )
+ORDER BY seller_name
