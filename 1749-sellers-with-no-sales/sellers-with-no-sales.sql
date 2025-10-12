be0c6e7737b6ae7400e9066e7379b21c2ec8d943
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-12 14:18:41 -0400

    Time: 837 ms (76.46%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,10 +1,9 @@
-select s.seller_name
-from
-    Seller s
-    left join Orders o on s.seller_id = o.seller_id
-where s.seller_id not in (
-    select seller_id
+select seller_name
+from Seller
+where not exists (
+    select 1
     from Orders
     where extract(year from sale_date) = 2020
+    and Seller.seller_id = Orders.seller_id
 )
 order by seller_name
