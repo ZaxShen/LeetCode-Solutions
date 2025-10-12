ef225ff9742962dc624f22f662f4731529e27c79
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-11 22:03:23 -0400

    Time: 450 ms (29.79%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,10 +1,11 @@
-SELECT 
-    ROUND(AVG((order_date = customer_pref_delivery_date)::INT) * 100, 2) AS immediate_percentage
-FROM (
+WITH cte AS (
     SELECT DISTINCT ON (customer_id)
         customer_id,
         order_date,
         customer_pref_delivery_date
     FROM Delivery
     ORDER BY customer_id, order_date
-) first_orders;
+)
+SELECT 
+    ROUND(AVG((order_date = customer_pref_delivery_date)::INT) * 100, 2) AS immediate_percentage
+FROM cte
