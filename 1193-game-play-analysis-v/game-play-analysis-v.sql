ac12b717de52d4f1859e0acfe74e51a50dc97ba6
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-12 22:40:04 -0400

    Time: 521 ms (43.59%) | Memory: 0B (100.00%) - LeetSync

--- 1193-game-play-analysis-v/game-play-analysis-v.sql
+++ 1193-game-play-analysis-v/game-play-analysis-v.sql
@@ -1,17 +1,16 @@
-with day1 as (
+with cte as (
     select
         player_id,
-        min(event_date) as day1,
-        min(event_date) + 1 as day2
+        min(event_date) as install_dt
     from Activity
     group by player_id
 )
 select
-    d.day1 as install_dt,
-    count(d.player_id) as installs,
-    round(count(a.player_id)::numeric / count(d.player_id), 2) as Day1_retention
-from day1 d
-    left join Activity a on d.player_id = a.player_id
-        and d.day2 = a.event_date
-group by d.day1
-order by d.day1
+    c.install_dt,
+    count(c.player_id) as installs,
+    round(count(a.player_id)::NUMERIC / count(c.player_id), 2) as Day1_retention
+from
+    cte c
+    left join Activity a on c.install_dt + 1 = a.event_date
+        and c.player_id = a.player_id
+group by c.install_dt
