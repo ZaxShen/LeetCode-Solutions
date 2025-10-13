4935fa64f1f7a059178529ff6b64b66bc963f80d
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-12 21:41:47 -0400

    Time: 332 ms (98.89%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,19 +1,14 @@
 with cte as (
     select
-        product_id,
-        new_price,
-        ROW_NUMBER() OVER(partition by product_id order by change_date desc) as rn
+        *,
+        dense_rank() over(partition by product_id order by change_date desc) as change_date_desc
     from Products
     where change_date <= '2019-08-16'::DATE
 )
-select
-    product_id,
-    new_price as price
+select product_id, new_price as price
 from cte
-where rn = 1
+where change_date_desc = 1
 union
-select
-    product_id,
-    10 as price
+select product_id, 10 as price
 from Products
-where product_id not in (select distinct product_id from cte)
+where product_id not in (select product_id from cte)
