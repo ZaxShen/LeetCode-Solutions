f61ffd9c6293f8c79c04b3c45a56190e205da043
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-16 22:45:33 -0400

    Time: 200 ms (44.68%) | Memory: 0B (100.00%) - LeetSync

--- 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
+++ 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
@@ -1,24 +1,25 @@
-with bins as (
-    select '[0-5>' as bin, 1 as bin_order
-    union all select '[5-10>', 2
-    union all select '[10-15>', 3
-    union all select '15 or more', 4
-),
-categorized as (
-    select
-        *,
-        case
-            when duration/60 < 5 THEN '[0-5>'
-            when duration/60 < 10 THEN '[5-10>'
-            when duration/60 < 15 THEN '[10-15>'
-            else '15 or more'
-        end as bin
-    from Sessions
+with bins(bin, "order") as (
+select '[0-5>', 1
+union
+select '[5-10>', 2
+union
+select '[10-15>', 3
+union
+select '15 or more', 4
+), stats as (
+select
+    case
+        when duration / 60 < 5 then '[0-5>'
+        when duration / 60 < 10 then '[5-10>'
+        when duration / 60 < 15 then '[10-15>'
+        else '15 or more'
+    end as bin
+from Sessions
 )
 select
     b.bin,
-    count(c.session_id) as total
+    count(s.bin) as total
 from bins b
-left join categorized c on b.bin = c.bin
-group by b.bin, b.bin_order
-order by b.bin_order
+left join stats s on b.bin = s.bin
+group by b.bin, b."order"
+order by b."order"
