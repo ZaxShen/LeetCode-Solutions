d0283ebaa7f1b145037866253e302e7c5e70ca7f
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-18 23:47:16 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (77.64%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,11 +1,13 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        asc = []
+        non_desc_stack = []
 
-        for i, v in enumerate(prices):
-            while asc and prices[asc[-1]] >= v:
-                j = asc.pop()
-                prices[j] -= v
-            asc.append(i)
+        for idx, i in enumerate(prices):
+            while non_desc_stack and prices[non_desc_stack[-1]] >= i:
+                _ = non_desc_stack.pop()
+                discount_price = prices[_] - i
+                prices[_] = discount_price
+            non_desc_stack.append(idx)
 
         return prices
+            
