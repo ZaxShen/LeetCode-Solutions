5835f62ae1fb46790dc3a755a4009fc74e536160
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-19 08:32:13 -0400

    Time: 3 ms (42.28%) | Memory: 18 MB (64.43%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,19 +1,15 @@
 class Solution:
-    # O(n), O(n)
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mono_stack = []
         hashmap = {}
-        desc_stack = []
-
         for i in nums2:
-            while desc_stack and desc_stack[-1] < i:
-                prev_greatest = desc_stack.pop()
-                hashmap[prev_greatest] = i
-            desc_stack.append(i)
+            while mono_stack and mono_stack[-1] < i:
+                hashmap[mono_stack.pop()] = i
+            mono_stack.append(i)
 
         res = [-1] * len(nums1)
-
         for idx, i in enumerate(nums1):
             if i in hashmap:
                 res[idx] = hashmap[i]
-        
+
         return res
