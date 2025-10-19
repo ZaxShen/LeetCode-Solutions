a0bd7506fdb534ff907781f7876135a45377dac6
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-18 23:31:48 -0400

    Time: 0 ms (100.00%) | Memory: 36.7 MB (74.59%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,8 +1,18 @@
 class Solution:
     # O(m + n), O(1)
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        intersection = set(nums1) & set(nums2)
+        # Early Termination: check if there is an overlap
+        if nums1[-1] < nums2[0] or nums2[-1] < nums1[0]:
+            return -1
+        
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            num1, num2 = nums1[i], nums2[j]
+            if num1 == num2:
+                return num1
+            elif num1 < num2:
+                i += 1
+            else:
+                j += 1
 
-        if intersection:
-            return min(intersection)
         return -1
