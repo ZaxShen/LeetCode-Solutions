aaa45d7fb55332dbdf0b87990bb21efcb8bfdf22
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-18 23:16:02 -0400

    Time: 0 ms (100.00%) | Memory: 37.3 MB (29.07%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,13 +1,16 @@
 class Solution:
+    # O(m + n), O(1)
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        # Early Termination
         if nums1[-1] < nums2[0] or nums2[-1] < nums1[0]:
             return -1
         
         i = j = 0
         while i < len(nums1) and j < len(nums2):
-            if nums1[i] == nums2[j]:
-                return nums1[i]
-            elif nums1[i] < nums2[j]:
+            n1, n2 = nums1[i], nums2[j]
+            if n1 == n2:
+                return n1
+            elif n1 < n2:
                 i += 1
             else:
                 j += 1
