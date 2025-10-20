67bd80a449f5815c1fb8b65631ac4fe93000b8f9
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-20 17:36:35 -0400

    Time: 232 ms (84.63%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,10 @@
+with cte as (
+    select
+        customer_number,
+        dense_rank() over(order by count(order_number) desc) as orders_rnk
+    from Orders
+    group by customer_number
+)
 select customer_number
-from Orders
-group by customer_number
-order by count(order_number) desc
-limit 1
+from cte
+where orders_rnk = 1
