45a8e97e2ba227ac06e372f6b9fe1b36e895bee6
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-20 21:06:55 -0400

    Time: 9 ms (58.31%) | Memory: 28.4 MB (10.94%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,15 +1,13 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left = 0
-        win_sum = 0
-        min_len = float('inf')
+        left = win_sum = 0
+        res = float('inf')
 
-        for right, num in enumerate(nums):
-            win_sum += num
-
+        for right, i in enumerate(nums):
+            win_sum += i
             while win_sum >= target:
-                min_len = min(min_len, right - left + 1)
+                res = min(res, right - left + 1)
                 win_sum -= nums[left]
                 left += 1
-            
-        return min_len if min_len != float('inf') else 0
+
+        return res if res != float('inf') else 0
