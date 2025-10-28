bc09be2ee86979dbbd3812b9ee7da301e5907509
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-27 23:50:09 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -6,7 +6,7 @@ class Solution:
         for i in s:
             if i in mapping:
                 stack.append(i)
-            elif not stack or mapping.get(stack.pop()) != i:
+            elif not stack or mapping[stack.pop()] != i:
                 return False
 
         return len(stack) == 0
