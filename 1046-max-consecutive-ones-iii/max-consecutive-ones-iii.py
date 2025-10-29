d348691a7f8088611ceea3dea768d6a89c391825
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-29 00:02:00 -0400

    Time: 63 ms (88.38%) | Memory: 20.1 MB (98.16%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,16 +1,16 @@
 class Solution:
     # O(n), O(1)
     def longestOnes(self, nums: List[int], k: int) -> int:
-        left = res = 0
+        left = max_len = 0
+        # Clever approach - window size only grows or stays same
+        for right, num in enumerate(nums):
+            if num == 0:  # Found a 0
+                k -= 1  # Use one flip
 
-        for right, i in enumerate(nums):
-            if i == 0:
-                k -= 1
-            
-            if k < 0:
-                k += nums[left] == 0
-                left += 1
+            if k < 0:  # Window becomes invalid
+                k += nums[left] == 0  # If left pointer is on 0, get back one flip
+                left += 1  # Always move right (maintain size)
             else:
-                res = max(res, right - left + 1)
+	            max_len = max(max_len, right - left + 1)
 
-        return res
+        return max_len
