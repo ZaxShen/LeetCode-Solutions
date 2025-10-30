8f1d918de5f6ad8df1d4a3f5912a840a45c3f87c
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-10-29 23:29:55 -0400

    Time: 482 ms (39.25%) | Memory: 33.7 MB (33.53%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,19 +1,14 @@
 from collections import defaultdict
 
 class Solution:
-    # O(n), O(k)
     def maximumSum(self, nums: List[int]) -> int:
         d = defaultdict(int)
         res = -1
 
         for i in nums:
             k = sum(map(int, str(i)))
-
             if k in d:
-                res = max(res, d[k] + i)
-            
+                res = max(res, i + d[k])
             d[k] = max(d[k], i)
 
         return res
-
-
