c13c0a205d45b2e434d03864092c26bdec662379
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-02 12:33:31 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (15.77%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,7 +1,6 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
-        if len(pattern) != len(s):
-            return False
-        
+        if len(pattern) != len(s): return False
+
         return tuple(map(pattern.index, pattern)) == tuple(map(s.index, s))
