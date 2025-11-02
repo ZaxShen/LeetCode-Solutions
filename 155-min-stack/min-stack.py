df2045e484715f64645e70bf2ba53731843b587d
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-01 23:31:47 -0400

    Time: 2 ms (86.23%) | Memory: 21 MB (99.77%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -8,7 +8,7 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if self.min_stack != []:
+        if self.min_stack:
             val = min(val, self.getMin())
         self.min_stack.append(val)
 
