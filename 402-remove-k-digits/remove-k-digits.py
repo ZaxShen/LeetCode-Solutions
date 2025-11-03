d91c8536bb97ee3d0066ac0d9b6c89edb7aea179
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-03 18:21:08 -0500

    Time: 7 ms (99.06%) | Memory: 19.1 MB (25.82%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -1,20 +1,17 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        # Edge Case: If k >= length, remove all digits
         if k >= len(num): return '0'
 
         mono_stack = []
 
         for i in num:
-            while k > 0 and mono_stack and mono_stack[-1] > i:
+            while k and mono_stack and mono_stack[-1] > i:
                 mono_stack.pop()
                 k -= 1
             mono_stack.append(i)
 
-        # Edge Case: Remove remaining k digits from end
         if k > 0: mono_stack = mono_stack[:-k]
 
-        # Edge Case: Handle leading zeros and empty result
         res = ''.join(mono_stack).lstrip('0') or '0'
-        
+
         return res
