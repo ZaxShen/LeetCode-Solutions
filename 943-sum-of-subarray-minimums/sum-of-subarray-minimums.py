219b292efb516f1de5f23bae7556662a990aa11f
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-03 17:57:48 -0500

    Time: 55 ms (89.95%) | Memory: 22 MB (32.08%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,17 +1,17 @@
 class Solution:
-    # O(n), O(n)
     def sumSubarrayMins(self, arr: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        res = [0] * len(arr)
         mono_stack = []
+        res = [0] * len(arr)
 
         for idx, i in enumerate(arr):
             while mono_stack and arr[mono_stack[-1]] > i:
                 mono_stack.pop()
 
             if mono_stack:
-                prev_min = res[mono_stack[-1]]
-                res[idx] = prev_min + i * (idx - mono_stack[-1])
+                # prev_sum = res[idx - 1]
+                prev_sum = res[mono_stack[-1]]
+                res[idx] = prev_sum + i * (idx - mono_stack[-1])
             else:
                 res[idx] = i * (idx + 1)
 
