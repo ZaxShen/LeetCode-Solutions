2d4ebf5dbb5bfaef17bf4bf197593b48d3d056d7
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-09 12:33:00 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (39.75%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,20 @@
+from collections import defaultdict
+
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = defaultdict(int)
         mono_stack = []
-        hashmap = {}
-        for i in nums2:
+
+        for idx, i in enumerate(nums2):
             while mono_stack and mono_stack[-1] < i:
                 hashmap[mono_stack.pop()] = i
             mono_stack.append(i)
 
-        res = [-1] * len(nums1)
-        for idx, i in enumerate(nums1):
+        res = []
+        for i in nums1:
             if i in hashmap:
-                res[idx] = hashmap[i]
+                res.append(hashmap[i])
+            else:
+                res.append(-1)
 
         return res
