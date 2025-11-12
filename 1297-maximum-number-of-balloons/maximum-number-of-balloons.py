9d04e9aa0025c231f5ecabd0585f621cc74519da
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-11 22:43:08 -0500

    Time: 3 ms (57.48%) | Memory: 17.8 MB (48.55%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         balloon = {
-            "b": 0,
-            "a": 0,
-            "l": 0,
-            "o": 0,
-            "n": 0
+            'b': 0,
+            'a': 0,
+            'l': 0,
+            'o': 0,
+            'n': 0
         }
 
-        for char in text:
-            if char in balloon:
-                balloon[char] += 1
+        for i in text:
+            if i in balloon:
+                balloon[i] += 1
 
-        ban = min(balloon['b'], balloon['a'], balloon['n'])
-        lo = min(balloon['l'], balloon['o']) // 2
-        
-        return min(ban, lo)
+        one_letter = balloon[min(balloon, key=balloon.get)]
+        two_letter = min(balloon['l'], balloon['o']) // 2
+
+        return min(one_letter, two_letter)
