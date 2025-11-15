2148a243d8fbba9866bce8f42e6762148485ffaf
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-15 00:07:30 -0500

    Time: 3 ms (14.37%) | Memory: 17.9 MB (17.51%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,3 +1,19 @@
+from collections import Counter
+
 class Solution:
+    # O(n), O(1)
     def customSortString(self, order: str, s: str) -> str:
-        return ''.join(sorted(s, key = lambda x: order.find(x) if x in order else 0))
+        count = Counter(s)
+        res = []
+
+        for i in order:
+            if i in count:
+                res.append(i * count[i])
+                del count[i]
+
+        for k, v in count.items():
+            res.append(k * v)
+
+        return ''.join(res)
+
+        
