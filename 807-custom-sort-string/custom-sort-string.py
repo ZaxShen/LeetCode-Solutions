62369b8453a2d09936b1bfd299b8e7398d8371d7
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-15 00:03:18 -0500

    Time: 3 ms (14.37%) | Memory: 17.7 MB (61.29%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,3 +1,3 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        return "".join(sorted(s, key=lambda x: order.find(x) if x in order else 0))
+        return ''.join(sorted(s, key = lambda x: order.find(x) if x in order else 0))
