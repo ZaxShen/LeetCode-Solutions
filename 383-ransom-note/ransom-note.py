8463d50681c2b3132018aa31726bcd8b59064a65
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-15 10:02:07 -0500

    Time: 15 ms (75.75%) | Memory: 17.9 MB (54.01%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,13 +2,4 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(ransomNote) > len(magazine):
-            return False
-
-        count = Counter(magazine)
-        for char in ransomNote:
-            count[char] -= 1
-            if count[char] < 0:
-                return False
-
-        return True
+        return Counter(magazine) >= Counter(ransomNote)
