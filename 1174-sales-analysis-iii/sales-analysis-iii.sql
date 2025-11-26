8a486cc94b40425bbec58e3437c123c7642cdd8b
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-11-26 14:02:58 -0500

    Time: 1042 ms (20.86%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,8 +1,12 @@
-select
-    s.product_id,
+select distinct
+    s1.product_id,
     p.product_name
 from
-    Sales s
-    join Product p on s.product_id = p.product_id
-group by s.product_id, p.product_name
-having min(s.sale_date) >= '2019-01-01'::date and max(s.sale_date) <= '2019-03-31'::date
+    Sales s1
+    join Product p on s1.product_id = p.product_id
+where not exists (
+    select 1
+    from Sales s2
+    where s1.product_id = s2.product_id
+    and (s2.sale_date < '2019-01-01'::date or s2.sale_date >= '2019-04-01'::date)
+)
