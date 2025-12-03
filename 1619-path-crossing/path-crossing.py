437348daf521c49954be46de9c04110a73a2355e
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-03 16:13:18 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (73.35%) - LeetSync

--- 1619-path-crossing/path-crossing.py
+++ 1619-path-crossing/path-crossing.py
@@ -1,19 +1,18 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        # x, y = 0, 0
         x = y = 0
-        visited = {(x, y)}
+        visited = {(x, y)} # Init visited with the original point
 
-        for move in path:
-            match move:
+        for i in path:
+            match i:
                 case 'N': y += 1
                 case 'S': y -= 1
                 case 'E': x += 1
                 case 'W': x -= 1
 
-            pos = (x, y)
-            if pos in visited:
+            loc = (x, y)
+            if loc in visited:
                 return True
-            visited.add(pos)
+            visited.add(loc)
 
         return False
