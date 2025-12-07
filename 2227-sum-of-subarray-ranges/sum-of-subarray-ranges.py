7c09adfa6840d0540dd477445366300597eaaa5e
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-07 16:46:48 -0500

    Time: 11 ms (95.95%) | Memory: 18 MB (45.82%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -1,31 +1,37 @@
 class Solution:
+    # O(n), O(n)
     def subArrayRanges(self, nums: List[int]) -> int:
         asc_stack = []
         desc_stack = []
         mins = [0] * len(nums)
         maxs = [0] * len(nums)
+        res = 0
 
         for idx, i in enumerate(nums):
             while asc_stack and nums[asc_stack[-1]] > i:
                 asc_stack.pop()
             
             if asc_stack:
-                prev_sum = mins[asc_stack[-1]]
-                mins[idx] = prev_sum + i * (idx - asc_stack[-1])
+                prev_min = mins[asc_stack[-1]]
+                curr_min = prev_min + i * (idx - asc_stack[-1])
             else:
-                mins[idx] = i * (idx + 1)
-
+                curr_min = i * (idx + 1)
+            
+            mins[idx] = curr_min
             asc_stack.append(idx)
 
             while desc_stack and nums[desc_stack[-1]] < i:
                 desc_stack.pop()
 
             if desc_stack:
-                prev_sum = maxs[desc_stack[-1]]
-                maxs[idx] = prev_sum + i * (idx - desc_stack[-1])
+                prev_max = maxs[desc_stack[-1]]
+                curr_max = prev_max + i * (idx - desc_stack[-1])
             else:
-                maxs[idx] = i * (idx + 1)
+                curr_max = i * (idx + 1)
 
+            maxs[idx] = curr_max
             desc_stack.append(idx)
 
-        return sum(i - j for i, j in zip(maxs, mins))
+            res += curr_max - curr_min
+
+        return res
