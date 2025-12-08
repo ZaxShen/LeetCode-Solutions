595052c7362c7d4569e769e0790562e88a81bb03
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-07 22:00:21 -0500

    Time: 3 ms (93.66%) | Memory: 19 MB (15.73%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -6,13 +6,11 @@ class Solution:
             if i > 0:
                 stack.append(i)
             else:
-                while stack and stack[-1] > 0 and stack[-1] < abs(i):
+                while stack and stack[-1] > 0 and abs(stack[-1]) < abs(i):
                     stack.pop()
-                if stack and stack[-1] == abs(i):
+                if stack and stack[-1] > 0 and abs(stack[-1]) == abs(i):
                     stack.pop()
-                elif not stack or stack[-1] < abs(i):
+                elif not stack or stack[-1] < 0:
                     stack.append(i)
 
         return stack
-
-        
