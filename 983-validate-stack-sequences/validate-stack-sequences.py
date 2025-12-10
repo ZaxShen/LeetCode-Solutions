d3b0ce558177c158da3c6e2bab563d1f0d2050ec
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-09 22:34:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (43.70%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -1,17 +1,15 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        # Edge Case
         if len(pushed) != len(popped): return False
-
+        
         stack = []
         j = 0
-
+        
         for i in pushed:
             stack.append(i)
             while stack and stack[-1] == popped[j]:
                 stack.pop()
                 j += 1
-
-        return not stack
-
+        
+        return len(stack) == 0
         
