5371fa940ed404c6b8e6a530cab8f153d58396f2
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-11 15:50:33 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.64%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,13 +1,17 @@
 class Solution:
+    # O(n), O(1)
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        min_len = len(strs[0])
-        for i in strs: min_len = min(min_len, len(i))
+        # Edge Case
+        # if len(strs) == 1: return strs[0]
+
+        # Early Termination
+        min_len = min([len(i) for i in strs])
 
         for i in range(min_len):
-            word = strs[0][i]
-            for j in strs[1:]:
-                if word != j[i]:
+            char = strs[0][i]
+            for word in strs[1:]:
+                if word[i] != char:
                     return strs[0][:i]
 
+        # Handle edge cases like [""], ["a", "ab"]
         return strs[0][:min_len]
-                
