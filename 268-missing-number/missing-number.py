2c41473937d6e6ef2ff682ea7a4767a2a4686fb8
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-11 01:06:14 -0500

    Time: 7 ms (37.39%) | Memory: 19.1 MB (6.99%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,4 +1,9 @@
 class Solution:
-    # O(n), O(n)
+    # O(n), O(1)
     def missingNumber(self, nums: List[int]) -> int:
-        return sum(range(len(nums) + 1)) - sum(nums)
+        all_nums = range(len(nums) + 1)
+        hashset = set(nums)
+
+        for i in all_nums:
+            if i not in hashset:
+                return i
