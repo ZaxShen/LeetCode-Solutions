8a28ec24d205615dec278eb78ef85fb5399ade6d
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-11 01:13:24 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (19.39%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(nums) + 1
         max_num = len(nums)
 
-        exp_sum = (0 + max_num) * (n / 2)
+        exp_sum = int((0 + max_num) * (n / 2))
         act_sum = sum(nums)
 
-        return int(exp_sum - act_sum)
+        return exp_sum - act_sum
