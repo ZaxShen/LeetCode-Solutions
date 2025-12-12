b8bd27e92a1bc8282dd15a67ffc3068cc920d785
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-12 12:01:06 -0500

    Time: 104 ms (55.04%) | Memory: 24.5 MB (37.52%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,19 +1,18 @@
 from collections import defaultdict
 
 class Solution:
-    # O(n), O(n)
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         seen = defaultdict(int)
         seen[0] = 1
-        res = prefix = 0
+        prefix = res = 0
 
-        for num in nums:
-            prefix += num % 2
-            target = prefix - k
+        for i in nums:
+            prefix += i % 2
+            lookup = prefix - k
 
-            if target in seen:
-                res += seen[target]
-
+            if lookup in seen:
+                res += seen[lookup]
+            
             seen[prefix] += 1
 
         return res
