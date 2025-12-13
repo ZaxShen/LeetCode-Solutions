0dea9fe594a6dcfd75aae9d29dd725979c3668b1
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-13 13:33:32 -0500

    Time: 2 ms (22.02%) | Memory: 17.7 MB (85.22%) - LeetSync

--- 1391-counting-elements/counting-elements.py
+++ 1391-counting-elements/counting-elements.py
@@ -1,12 +1,26 @@
-from collections import Counter
+from collections import defaultdict
 
 class Solution:
     def countElements(self, arr: List[int]) -> int:
-        res = 0
-        count = Counter(arr)
-
-        for i in count:
-            if i + 1 in count:
-                res += count[i]
-
-        return res
+        seen = set()
+        pending = defaultdict(int)
+        ans = 0
+        
+        for x in arr:
+            # CHECK 1: Is x valid right now?
+            # If we have already seen x+1, then this x is valid immediately.
+            if (x + 1) in seen:
+                ans += 1
+            else:
+                # If not, mark this x as "waiting" for a future x+1
+                pending[x] += 1
+            
+            # CHECK 2: Does x validate previous numbers?
+            # If this is the first time we see x, it satisfies all pending (x-1)s.
+            if x not in seen:
+                if (x - 1) in pending:
+                    ans += pending[x - 1]
+                    pending[x - 1] = 0  # Clear them, they are counted now
+                seen.add(x)
+                
+        return ans
