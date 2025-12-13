f53c7e3bbca1b99e31f6fb51bf8247a003cbbf94
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-13 13:26:38 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (13.10%) - LeetSync

--- 1391-counting-elements/counting-elements.py
+++ 1391-counting-elements/counting-elements.py
@@ -1,12 +1,12 @@
 from collections import Counter
 
 class Solution:
-    # O(n), O(k)
     def countElements(self, arr: List[int]) -> int:
-        count = Counter(arr)
         res = 0
+        count = Counter(arr)
 
-        for k, v in count.items():
-            if k + 1 in count: res += v
+        for i in count:
+            if i + 1 in count:
+                res += count[i]
 
         return res
