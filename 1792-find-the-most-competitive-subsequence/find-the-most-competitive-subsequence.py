7ace5b24b930ec776ec801bba39bb1d309088051
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-13 18:06:11 -0500

    Time: 63 ms (90.48%) | Memory: 30.8 MB (15.42%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
@@ -2,13 +2,13 @@ class Solution:
     # O(n), O(1)
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
         asc_stack = []
-        # Use stack_size to restrict the nums[-k:]
-        stack_size = len(nums) - k
+        # Define how many items we can remove
+        count = len(nums) - k
 
         for i in nums:
-            while stack_size and asc_stack and asc_stack[-1] > i:
+            while count and asc_stack and asc_stack[-1] > i:
                 asc_stack.pop()
-                stack_size -= 1
+                count -= 1
             asc_stack.append(i)
 
         return asc_stack[:k]
