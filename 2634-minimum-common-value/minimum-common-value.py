358b15d959fce6b9b66f864e572c011aaeb31e40
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-16 13:41:39 -0500

    Time: 0 ms (100.00%) | Memory: 36.2 MB (82.11%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,16 +1,15 @@
 class Solution:
-    # O(m + n), O(1)
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        # Early Termination: check if there is an overlap
+        # Edge Case
         if nums1[-1] < nums2[0] or nums2[-1] < nums1[0]:
             return -1
-        
+
         i = j = 0
+
         while i < len(nums1) and j < len(nums2):
-            num1, num2 = nums1[i], nums2[j]
-            if num1 == num2:
-                return num1
-            elif num1 < num2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
                 i += 1
             else:
                 j += 1
