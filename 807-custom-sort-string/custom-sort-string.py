238095add3c6d7fcf2660537273828b90bcb1e2f
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-16 13:27:36 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.91%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,18 +1,16 @@
+from collections import Counter
+
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        hashmap = {}
-        res = ""
-        for c in s:
-            if c in hashmap:
-                hashmap[c] += 1
-            else:
-                hashmap[c] = 1
-        #Add the character if it's present in hashmap to res
-        for char in order:
-            if char in hashmap:
-                res += char * hashmap[char]
-                del hashmap[char]
-        #Add the remaining characters
-        for char, count in hashmap.items():
-            res += char * count
-        return res
+        res = []
+        count = Counter(s)
+        
+        for i in order:
+            if i in count:
+                res.append(i * count[i])
+                del count[i]
+
+        for k, v in count.items():
+            res.append(k * v)
+
+        return ''.join(res)
