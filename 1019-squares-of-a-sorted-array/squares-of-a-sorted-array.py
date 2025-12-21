eba4d889da2c6b96e2980adb5a1078709c3a5f67
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-21 02:33:44 -0500

    Time: 9 ms (60.32%) | Memory: 19.4 MB (74.65%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,20 +1,19 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        if nums[0] >= 0:
-            return [i ** 2 for i in nums]
-        elif nums[-1] <= 0:
-            return [i ** 2 for i in nums][::-1]
+        if nums[0] >= 0: return [i ** 2 for i in nums]
+        elif nums[-1] <= 0: return [i ** 2 for i in nums][::-1]
 
         res = [0] * len(nums)
-        left, right = 0, len(nums) - 1
+        left = 0
+        right = len(nums) - 1
 
-        for i in range(len(nums) - 1, -1, -1):
-            abs_left, abs_right = abs(nums[left]), abs(nums[right])
-            if abs_left < abs_right:
-                res[i] = abs_right ** 2
+        # for i in range(len(nums), -1, -1):
+        for pointer in range(len(nums) - 1, -1, -1):
+            if abs(nums[right]) >= abs(nums[left]):
+                res[pointer] = nums[right] ** 2
                 right -= 1
             else:
-                res[i] = abs_left ** 2
+                res[pointer] = nums[left] ** 2
                 left += 1
 
         return res
