06e2711b0f6c5bf325d6c0771ae5da78640a3f95
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-24 14:40:03 -0500

    Time: 47 ms (80.13%) | Memory: 31.4 MB (97.39%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,5 +1,4 @@
 class Solution:
-    # O(n), O(1)
     def waysToSplitArray(self, nums: List[int]) -> int:
         total = sum(nums)
         left_sec = res = 0
@@ -7,6 +6,7 @@ class Solution:
         for i in nums[:-1]:
             left_sec += i
             right_sec = total - left_sec
-            if left_sec >= right_sec: res += 1
+            if left_sec >= right_sec:
+                res += 1
 
         return res
