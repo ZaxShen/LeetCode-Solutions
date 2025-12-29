6192b6ed4fe961ebc044ddd5b46c3308d8fd4566
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-29 18:17:12 -0500

    Time: 111 ms (32.13%) | Memory: 22.2 MB (97.77%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -7,11 +7,16 @@ class Solution:
         prefix = res = 0
 
         for idx, i in enumerate(nums):
-            prefix += 1 if i == 1 else -1
-            
+            if i == 1:
+                prefix += 1
+            else:
+                prefix -= 1
+
             if prefix in seen:
                 res = max(res, idx - seen[prefix])
+            # Store the first occurance for longest result
             else:
                 seen[prefix] = idx
 
+        print(seen)
         return res
