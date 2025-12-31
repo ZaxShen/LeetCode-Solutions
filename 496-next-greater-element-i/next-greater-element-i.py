48fcdc4fe04c949c5bb82ddc401c97dd8188b104
Zax S <24534796+ZaxShen@users.noreply.github.com>
2025-12-31 10:27:06 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,17 +1,16 @@
 class Solution:
-    # O(n), O(n)
+    # O(m+n), O(n)
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         mapping = {}
+        desc_stack = []
 
-        asc_stack = []
-        
+        # Record the next greater element in mapping
         for i in nums2:
-            while asc_stack and asc_stack[-1] < i:
-                mapping[asc_stack.pop()] = i
-            asc_stack.append(i)
+            while desc_stack and desc_stack[-1] < i:
+                mapping[desc_stack.pop()] = i
+            desc_stack.append(i)
 
         res = [-1] * len(nums1)
-        
         for idx, i in enumerate(nums1):
             if i in mapping:
                 res[idx] = mapping[i]
