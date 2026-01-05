814ad7835f0061bea95eea85fb4e36809d36f21a
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-01-05 01:54:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.97%) - LeetSync

--- 1098-largest-unique-number/largest-unique-number.py
+++ 1098-largest-unique-number/largest-unique-number.py
@@ -1,12 +1,13 @@
 from collections import Counter
 
 class Solution:
+    # O(n), O(k)
     def largestUniqueNumber(self, nums: List[int]) -> int:
         count = Counter(nums)
 
-        res = -1
+        res = -float('inf')
         for k, v in count.items():
             if v == 1:
                 res = max(res, k)
 
-        return res
+        return res if res != -float('inf') else -1
