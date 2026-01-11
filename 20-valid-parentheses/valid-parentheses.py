9e5c1e0b13d26e3f4736a300dd3ef4a2a18a5411
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-01-10 21:34:43 -0500

    Time: 3 ms (37.16%) | Memory: 19.4 MB (6.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,12 @@
 class Solution:
-    # O(n), O(1)
     def isValid(self, s: str) -> bool:
-        mapping = {"{": "}", "[": "]", "(": ")"}
+        mapping = {'(': ')', '[': ']', '{': '}'}
         stack = []
 
         for i in s:
-            if i in mapping:
+            if stack and mapping.get(stack[-1]) == i:
+                stack.pop()
+            else:
                 stack.append(i)
-            elif not stack or mapping[stack.pop()] != i:
-                return False
 
         return len(stack) == 0
