30e03246d2d55c48ec4cc13ee36aad1ac8524e96
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-01-10 21:19:40 -0500

    Time: 7 ms (60.46%) | Memory: 20.4 MB (5.64%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,13 +1,13 @@
 class Solution:
+    # O(n), O(1)
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        
-        # > <
         stack = []
 
         for i in asteroids:
             # ? >
             if i > 0:
                 stack.append(i)
+            # ? <
             else:
                 # > <<
                 while stack and stack[-1] > 0 and stack[-1] + i < 0:
@@ -16,10 +16,10 @@ class Solution:
                 if stack and stack[-1] + i == 0:
                     stack.pop()
                 # >> <
-                # elif stack and stack[-1] + i > 0:
-                #     continue
+                # continue, ignore this
                 # < <
                 elif not stack or stack[-1] < 0:
                     stack.append(i)
-
+                
         return stack
+        
