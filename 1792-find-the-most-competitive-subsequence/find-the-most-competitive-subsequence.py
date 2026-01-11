84bad65470d1a834d7e88fe6df28c6c94b90dd86
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-01-10 21:04:07 -0500

    Time: 47 ms (99.52%) | Memory: 30.9 MB (7.40%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
@@ -1,17 +1,13 @@
 class Solution:
     # O(n), O(1)
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
-        # Edge case
-        if k == 0: return nums
-
+        count = len(nums) - k
         stack = []
-        remove_count = len(nums) - k
-        
+
         for i in nums:
-            while remove_count and stack and stack[-1] > i:
+            while count and stack and stack[-1] > i:
                 stack.pop()
-                remove_count -= 1
+                count -= 1
             stack.append(i)
 
         return stack[:k]
-
