c065610744bd3892b11bc090ddde48b30eb28392
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-01-15 23:10:21 -0500

    Time: 336 ms (67.79%) | Memory: 33.6 MB (50.69%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,24 +1,14 @@
 class Solution:
-    # O(n), O(1)
     def maximumSum(self, nums: List[int]) -> int:
-        # O(n), O(1)
-        def helper(num: int) -> str:
-            res = 0
-            while num != 0:
-                res += num % 10
-                num = num // 10
+        d = {}
+        res = -1
 
-            return res + num
-
-        d = dict()
-        res = -float('inf')
-        
         for i in nums:
-            digits = helper(i)
+            digits = sum(map(int, str(i)))
             if digits in d:
-                res = max(res, d[digits] + i)
+                res = max(res, i + d[digits])
                 d[digits] = max(d[digits], i)
             else:
                 d[digits] = i
 
-        return res if res != -float('inf') else -1
+        return res
