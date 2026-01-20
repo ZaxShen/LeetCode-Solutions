9204fc14ac6da64de733ce90f6c82a60ff7f47dc
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-01-20 09:43:29 -0500

    Time: 1 ms (37.06%) | Memory: 19.5 MB (7.08%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,17 +1,12 @@
-class Solution:
-    # O(n), O(n)
-    def isSubsequence(self, s: str, t: str) -> bool:
-        # Early Termination
-        if len(s) > len(t) or set(s) > set(t):
-            return False
+from collections import Counter
 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
         i = j = 0
+
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
-            # Early Termination
-            if len(s) - i > len(t) - j:
-                return False
             j += 1
 
         return i == len(s)
