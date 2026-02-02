e19cb9ec68c8c842d505304b8a600ec2542637fb
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-02-01 20:56:57 -0500

    Time: 95 ms (38.92%) | Memory: 33.6 MB (29.33%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
@@ -1,13 +1,17 @@
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
-        desc_stack = []
         res = [0] * len(heights)
+        mono_stack = []
+
+        # 0 1 
+        # 1 0 0 0 0 0
 
         for idx, i in enumerate(heights):
-            while desc_stack and heights[desc_stack[-1]] < i:
-                res[desc_stack.pop()] += 1
-            if desc_stack:
-                res[desc_stack[-1]] += 1
-            desc_stack.append(idx)
+            
+            while mono_stack and heights[mono_stack[-1]] < i:
+                res[mono_stack.pop()] += 1
+            if mono_stack:
+                res[mono_stack[-1]] += 1
+            mono_stack.append(idx)
 
         return res
