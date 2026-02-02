0a1036bc80e20d4b48e9d0d8fd216a80ea25dd33
Zax S <24534796+ZaxShen@users.noreply.github.com>
2026-02-01 21:53:42 -0500

    Time: 85 ms (14.79%) | Memory: 24.1 MB (25.58%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,11 +1,12 @@
 class StockSpanner:
 
     def __init__(self):
-        self.mono_stack = [] # price, span
+        self.mono_stack = []  # price, span
 
     def next(self, price: int) -> int:
         mono_stack = self.mono_stack
         span = 1
+
         while mono_stack and mono_stack[-1][0] <= price:
             span += mono_stack.pop()[1]
         mono_stack.append([price, span])
